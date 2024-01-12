2d7fea135cc32e24cfa1977feb50283f3956c796
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-12 16:10:03 +0300

    Add an Entity configuration

---
+++ Coupon.Infrastructure/Configuration/CouponTypeConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Coupon.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Coupon.Infrastructure.Configuration
+{
+    public class CouponTypeConfiguration : IEntityTypeConfiguration<CouponEntity>
+    {
+        public void Configure(EntityTypeBuilder<CouponEntity> builder)
+        {
+            builder.ToTable("Coupons");
+            builder.HasKey(key => key.CouponId);
+
+            SeedData(builder);
+        }
+
+        private static void SeedData(EntityTypeBuilder<CouponEntity> builder)
+        {
+            builder.HasData(new CouponEntity
+            {
+                CouponId = 1,
+                CouponCode = "5OFF",
+                DiscountAmount = 2,
+                MinAmount = 7,
+            },
+
+            new CouponEntity
+            {
+                CouponId = 1,
+                CouponCode = "5OFF",
+                DiscountAmount = 5,
+                MinAmount = 10,
+            });
+        }
+    }
+}
