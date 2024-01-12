7be48e62ca35dc775caf80ec574f77c354546da9
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-12 16:16:55 +0300

    Implement the repository pattern

---
+++ Coupon.Infrastructure/Repository/CouponRepository.cs
@@ -0,0 +1,43 @@
+﻿using Coupon.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using PineappleSite.Coupon.Core.Interfaces;
+
+namespace Coupon.Infrastructure.Repository
+{
+    public class CouponRepository(ApplicationDbContext context) : ICouponRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<IEnumerable<CouponEntity>> GetAllAsync()
+        {
+            return await _context.Coupons.ToListAsync();
+        }
+
+        public async Task<CouponEntity> GetAsync(int couponId)
+        {
+            return await _context.Coupons.FirstAsync(key => key.CouponId == couponId);
+        }
+
+        public async Task CreateAsync(CouponEntity coupon)
+        {
+            await _context.Coupons.AddAsync(coupon);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(CouponEntity coupon)
+        {
+            _context.Entry(coupon).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int couponId)
+        {
+            var coupon = await _context.Coupons.FirstAsync(key => key.CouponId == couponId);
+            if (coupon != null)
+            {
+                _context.Coupons.Remove(coupon);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
