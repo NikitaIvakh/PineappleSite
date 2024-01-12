5c62bd7a13926a375896e862dbddcbd227fbac59
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-12 16:26:41 +0300

    Add Mapping

---
+++ Coupon.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Coupon.Application.DTOs;
+using Coupon.Core.Entities;
+
+namespace Coupon.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CouponEntity, CouponDto>().ReverseMap();
+        }
+    }
+}
