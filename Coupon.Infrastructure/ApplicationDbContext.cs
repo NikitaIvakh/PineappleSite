ab7dae5ec4092ad985cac40fdee12bd946698a00
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-12 16:07:07 +0300

    Add a database context

---
+++ Coupon.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Coupon.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coupon.Infrastructure
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<CouponEntity> Coupons { get; set; }
+    }
+}
