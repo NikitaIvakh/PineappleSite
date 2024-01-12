aa4b0f24a5fc3f8378ee552b9c10abcddd573079
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-12 16:34:21 +0300

    Add a request to receive a list of coupons

---
+++ Coupon.Application/Features/Coupons/Handlers/Queries/GetCouponListRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Coupon.Application.DTOs;
+using Coupon.Application.Features.Coupons.Requests.Queries;
+using MediatR;
+using PineappleSite.Coupon.Core.Interfaces;
+
+namespace Coupon.Application.Features.Coupons.Handlers.Queries
+{
+    public class GetCouponListRequestHandler(ICouponRepository repository, IMapper mapper) : IRequestHandler<GetCouponListRequest, IEnumerable<CouponDto>>
+    {
+        private readonly ICouponRepository _repository = repository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<IEnumerable<CouponDto>> Handle(GetCouponListRequest request, CancellationToken cancellationToken)
+        {
+            var coupons = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<CouponDto>>(coupons);
+        }
+    }
+}
