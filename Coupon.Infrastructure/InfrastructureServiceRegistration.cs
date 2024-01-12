bd7691fd70e7889c961e0f5356a264335e6eb93a
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-12 16:13:14 +0300

    Add a class for database registration

---
+++ Coupon.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Coupon.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServive(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CouponConnectionString"));
+            });
+
+            var score = services.BuildServiceProvider();
+            using (var serviceProvider = score.CreateScope())
+            {
+                var context = serviceProvider.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+
+            return services;
+        }
+    }
+}
