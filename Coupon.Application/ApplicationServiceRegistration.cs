9e6ff2837816394a5f9d2b0217ddb254f8c537be
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-12 16:28:28 +0300

    Register mapping

---
+++ Coupon.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Coupon.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection ConfigureApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
