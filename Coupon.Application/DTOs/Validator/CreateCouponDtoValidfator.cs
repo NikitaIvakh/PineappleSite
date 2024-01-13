f3b37caf02505d7dd774d5feecc4eb11a114d449
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-13 10:15:10 +0300

    Add validation rules to create a coupon

---
+++ Coupon.Application/DTOs/Validator/CreateCouponDtoValidfator.cs
@@ -0,0 +1,16 @@
+﻿using Coupon.Application.Interfaces;
+using FluentValidation;
+
+namespace Coupon.Application.DTOs.Validator
+{
+    public class CreateCouponDtoValidfator : AbstractValidator<CreateCouponDto>
+    {
+        private readonly ICouponDbContext _context;
+
+        public CreateCouponDtoValidfator(ICouponDbContext context)
+        {
+            _context = context;
+            Include(key => new ICouponDtoValidfator(_context));
+        }
+    }
+}
