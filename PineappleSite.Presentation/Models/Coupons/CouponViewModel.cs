5e7f521c22da09e4aab7e5097c30791793e942d2
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-13 12:51:17 +0300

    Create the CouponService class. Create a CouponViewModel class

---
+++ PineappleSite.Presentation/Models/Coupons/CouponViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PineappleSite.Presentation.Models.Coupons
+{
+    public class CouponViewModel
+    {
+        public int CouponId { get; set; }
+
+        public string CouponCode { get; set; }
+
+        public double DiscountAmount { get; set; }
+
+        public double MinAmount { get; set; }
+    }
+}
