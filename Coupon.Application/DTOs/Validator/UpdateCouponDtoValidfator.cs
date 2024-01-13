2d3a5c28d25515d30a2682d8a3114e69718928d6
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-13 10:17:33 +0300

    Add validation rules for coupon renewal

---
+++ Coupon.Application/DTOs/Validator/UpdateCouponDtoValidfator.cs
@@ -0,0 +1,17 @@
+﻿using Coupon.Application.Interfaces;
+using FluentValidation;
+
+namespace Coupon.Application.DTOs.Validator
+{
+    public class UpdateCouponDtoValidfator : AbstractValidator<UpdateCouponDto>
+    {
+        private readonly ICouponDbContext _context;
+
+        public UpdateCouponDtoValidfator(ICouponDbContext context)
+        {
+            _context = context;
+            RuleFor(key => key.CouponId).NotNull().NotEmpty();
+            Include(key => new ICouponDtoValidfator(_context));
+        }
+    }
+}
