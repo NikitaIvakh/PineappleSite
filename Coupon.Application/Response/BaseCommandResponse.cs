cdafde6b8d6c0a0fa0a616c3817731ae6bbebc61
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-13 10:21:40 +0300

    Add the BaseCommandResponse class

---
+++ Coupon.Application/Response/BaseCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Coupon.Application.Response
+{
+    public class BaseCommandResponse
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsSuccess { get; set; } = true;
+
+        public List<string> ValidationErrors { get; set; }
+    }
+}
