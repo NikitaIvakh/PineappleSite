2c3628a835eedf27da7b26d5484184ffa49b1c0e
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-15 11:32:00 +0300

    Add RegisterRequest class

---
+++ Identity.Core/Entities/Identities/RegisterRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Core.Entities.Identities
+{
+    internal class RegisterRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
