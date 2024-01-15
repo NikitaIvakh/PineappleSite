6d55c34295e758ed9d94c8b5d5dc5f0c91fc9f31
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-15 12:23:28 +0300

    Create mapping

---
+++ Identity.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Identity.Application.DTOs.Identity;
+using Identity.Core.Entities.Identities;
+
+namespace Identity.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AuthRequest, AuthRequestDto>().ReverseMap();
+            CreateMap<RegisterRequest, RegisterRequestDto>().ReverseMap();
+        }
+    }
+}
