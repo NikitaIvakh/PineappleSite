d02fe684ea2310d8071760479e7c8dc2652edfdf
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-15 12:26:11 +0300

    Register automapper

---
+++ Identity.Application/PineAppleApplicationRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Identity.Application
+{
+    public static class PineAppleApplicationRegistration
+    {
+        public static IServiceCollection ConfigureApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
