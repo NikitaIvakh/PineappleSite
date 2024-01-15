2e26fc993a1e5ffeba71a08158e275d9a9b02a11
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-15 16:22:35 +0300

    BaseIdentityService

---
+++ PineappleSite.Presentation/Services/Identities/BaseIdentityService.cs
@@ -0,0 +1,37 @@
+﻿using PineappleSite.Presentation.Contracts;
+using System.Net.Http.Headers;
+
+namespace PineappleSite.Presentation.Services.Identities
+{
+    public class BaseIdentityService(ILocalStorageService localStorageService, IIdentityClient identityClient)
+    {
+        private readonly ILocalStorageService _localStorageService = localStorageService;
+        private readonly IIdentityClient _identityClient = identityClient;
+
+        protected IdentityResponseViewModel ConvertIdentityExceptions(IdentityExceptions identityExceptions)
+        {
+            if (identityExceptions.StatusCode == 400)
+            {
+                return new IdentityResponseViewModel() { Message = "Произошли ошибки валидации.", ValidationErrors = identityExceptions.Response, IsSuccess = false };
+            }
+
+            else if (identityExceptions.StatusCode == 404)
+            {
+                return new IdentityResponseViewModel() { Message = "Требуемый элемент не удалось найти.", IsSuccess = false };
+            }
+
+            else
+            {
+                return new IdentityResponseViewModel() { Message = "Что-то пошло не так, пожалуйста, попробуйте еще раз.", IsSuccess = false };
+            }
+        }
+
+        protected void AddBearerToken()
+        {
+            if (_localStorageService.Exists("token"))
+            {
+                _identityClient.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _localStorageService.GetStorageValue<string>("token"));
+            }
+        }
+    }
+}
---
+++ PineappleSite.Presentation/Services/Identities/IdentityResponseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PineappleSite.Presentation.Services.Identities
+{
+    public class IdentityResponseViewModel
+    {
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsSuccess { get; set; } = true;
+
+        public string ValidationErrors { get; set; }
+
+        public object? Data { get; set; }
+    }
+}
