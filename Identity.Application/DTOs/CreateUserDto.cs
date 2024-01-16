ca4c69ff58f5e0e01e8168eddd6fe3fbcf1cbde0
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-16 10:41:52 +0300

    Add classes for crud operations

---
+++ Identity.Application/DTOs/CreateUserDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Core.Entities.Users
+{
+    public class CreateUserDto : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
--- Identity.Application/DTOs/Identities/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Identity.Application.DTOs.Identities
-{
-    public class ApplicationUser
-    {
-
-    }
-}
