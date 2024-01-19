c0234e8dd1d93bf7e8083bb73bbfc966edd076ce
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-19 10:06:25 +0300

    Create validation rule for DeleteUserListDto

---
+++ Identity.Application/DTOs/Validators/IDeleteUserListDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Identity.Application.DTOs.Identities;
+
+namespace Identity.Application.DTOs.Validators
+{
+    public class IDeleteUserListDtoValidator : AbstractValidator<DeleteUserListDto>
+    {
+        public IDeleteUserListDtoValidator()
+        {
+            RuleFor(key => key.UserIds).NotNull().NotEmpty().NotEqual(key => key.UserIds);
+        }
+    }
+}
