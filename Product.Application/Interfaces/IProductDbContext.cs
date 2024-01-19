0aa506c9243278bff2f7471e706b771de0223590
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-19 19:11:00 +0300

    Create IProductDbContext interface

---
+++ Product.Application/Interfaces/IProductDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Core.Entities.Producrs;
+
+namespace Product.Application.Interfaces
+{
+    public interface IProductDbContext
+    {
+        public DbSet<ProductEntity> Products { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
