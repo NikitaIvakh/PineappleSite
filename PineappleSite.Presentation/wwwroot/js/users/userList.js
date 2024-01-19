d6a7cb817fce88ecdddd20dd8b0505926d6f5247
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-19 11:17:28 +0300

    Update views. Update css files

---
+++ PineappleSite.Presentation/wwwroot/js/users/userList.js
@@ -0,0 +1,15 @@
+﻿document.getElementById("clearSearch").addEventListener("click", function () {
+    document.getElementById("searchUser").value = "";
+    window.location.href = "/User/Index";
+});
+
+$(document).ready(function () {
+    $('input[name="selectedUserIds"]').change(function () {
+        var selectedCount = $('input[name="selectedUserIds"]:checked').length;
+        if (selectedCount >= 2) {
+            $('#deleteAllButton').show();
+        } else {
+            $('#deleteAllButton').hide();
+        }
+    });
+});
