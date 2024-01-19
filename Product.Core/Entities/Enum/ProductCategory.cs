c95e2822b79f7c13c8ae55b10501446c712c712b
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-19 19:07:19 +0300

    Create ProductEntity entity and enum

---
+++ Product.Core/Entities/Enum/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Product.Core.Entities.Enum
+{
+    public enum ProductCategory
+    {
+        Soups = 1,
+        Snacks = 2,
+        Drinks = 3,
+    }
+}
---
+++ Product.Core/Entities/Producrs/ProductEntity.cs
@@ -0,0 +1,17 @@
+﻿using Product.Core.Entities.Enum;
+
+namespace Product.Core.Entities.Producrs
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+
+        public double Price { get; set; }
+    }
+}
