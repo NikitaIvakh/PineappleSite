3214d202f857919344af502fadcfc6dd8ead224f
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 10:09:15 +0300

    Add AutoMapper

---
+++ Product.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Product.Application.DTOs.Products;
+using Product.Core.Entities.Producrs;
+
+namespace Product.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region
+            CreateMap<ProductEntity, ProductDto>()
+                .ForMember(key => key.ProductCategory, opt => opt.MapFrom(key => key.ProductCategory))
+                .ReverseMap();
+            #endregion
+        }
+    }
+}
