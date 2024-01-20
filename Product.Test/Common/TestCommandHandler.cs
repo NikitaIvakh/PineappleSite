96e1f139ae1cb5e99311cfb3ea3e53b5e79c6c23
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 14:49:21 +0300

    Add a class for testing the command

---
+++ Product.Test/Common/TestCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Product.Application.Profiles;
+using Product.Infrastructure;
+
+namespace Product.Test.Common
+{
+    public class TestCommandHandler : IDisposable
+    {
+        protected PineAppleProductsDbContext Context;
+        protected IMapper Mapper;
+
+        public TestCommandHandler()
+        {
+            Context = ProductDbContextFactory.Create();
+
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            ProductDbContextFactory.Destroy(Context);
+        }
+    }
+}
