9780f9bdad9975bc2399f9b949cebb979b30011f
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 16:11:03 +0300

    Add a product removal test

---
+++ Product.Test/Commands/DeleteProductDtoRequestHandlerTest.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Product.Application.DTOs.Products;
+using Product.Application.Features.Commands.Handlers;
+using Product.Application.Features.Requests.Handlers;
+using Product.Test.Common;
+using Shouldly;
+using Xunit;
+
+namespace Product.Test.Commands
+{
+    public class DeleteProductDtoRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task DeleteProductDtoRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new DeleteProductDtoRequestHandler(Context, Mapper);
+            var deleteProductDto = new DeleteProductDto
+            {
+                Id = 3,
+            };
+
+            // Act
+            var result = await handler.Handle(new DeleteProductDtoRequest
+            {
+                DeleteProduct = deleteProductDto
+            }, CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<Unit>();
+        }
+
+        [Fact]
+        public async Task DeleteProductDtoRequestHandlerTest_FailOrWrong()
+        {
+            // Arrange
+            var handler = new DeleteProductDtoRequestHandler(Context, Mapper);
+            var deleteProductDto = new DeleteProductDto
+            {
+                Id = 999,
+            };
+
+            // Act &7 Assert
+            await Assert.ThrowsAsync<Exception>(async () => await handler.Handle(new DeleteProductDtoRequest
+            {
+                DeleteProduct = deleteProductDto
+            }, CancellationToken.None));
+        }
+    }
+}
