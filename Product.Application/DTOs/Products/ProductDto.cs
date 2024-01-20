3ff90aee33dde9cf6159d964c22bfd46bc6fb5aa
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 10:07:05 +0300

    Create ProductDto class

---
+++ Product.Application/DTOs/Products/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using Product.Core.Entities.Enum;
+
+namespace Product.Application.DTOs.Products
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+
+        public double Price { get; set; }
+    }
+}
