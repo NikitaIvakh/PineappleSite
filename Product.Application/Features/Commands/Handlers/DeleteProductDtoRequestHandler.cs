7fab262abcbe828c0f7208d07fa1b9bf5e29f928
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 16:06:32 +0300

    Add a product removal command

---
+++ Product.Application/Features/Commands/Handlers/DeleteProductDtoRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Product.Application.Features.Requests.Handlers;
+using Product.Application.Interfaces;
+
+namespace Product.Application.Features.Commands.Handlers
+{
+    public class DeleteProductDtoRequestHandler(IProductDbContext context, IMapper mapper) : IRequestHandler<DeleteProductDtoRequest, Unit>
+    {
+        private readonly IProductDbContext _context = context;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Unit> Handle(DeleteProductDtoRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FindAsync(new object[] { request.DeleteProduct.Id }, cancellationToken)
+                ?? throw new Exception($"Продукт с идентификатором ({request.DeleteProduct.Id}) не существует");
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Product.Application/Features/Requests/Handlers/DeleteProductDtoRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Product.Application.DTOs.Products;
+
+namespace Product.Application.Features.Requests.Handlers
+{
+    public class DeleteProductDtoRequest : IRequest<Unit>
+    {
+        public DeleteProductDto DeleteProduct { get; set; }
+    }
+}
