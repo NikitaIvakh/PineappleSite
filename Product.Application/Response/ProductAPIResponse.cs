108cab5088b2513969de92a20e7eda5c8576d434
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 19:11:18 +0300

    Create ProductAPIResponse class

---
+++ Product.Application/Response/ProductAPIResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Product.Application.Response
+{
+    public class ProductAPIResponse
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsSuccess { get; set; } = true;
+
+        public List<string> ValidationErrors { get; set; }
+    }
+}
