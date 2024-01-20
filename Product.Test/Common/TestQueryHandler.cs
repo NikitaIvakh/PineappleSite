3ef9e72d8f880f1978fbbf6b0a232288d836aff6
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 10:41:50 +0300

    Create class for tests queries

---
+++ Product.Test/Common/TestQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Product.Application.Profiles;
+using Product.Infrastructure;
+using Xunit;
+
+namespace Product.Test.Common
+{
+    public class TestQueryHandler : IDisposable
+    {
+        protected PineAppleProductsDbContext Context;
+        protected IMapper Mapper;
+
+        public TestQueryHandler()
+        {
+            Context = ProductDbContextFactory.Create();
+
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            ProductDbContextFactory.Destroy(Context);
+        }
+    }
+
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<TestQueryHandler> { }
+}
