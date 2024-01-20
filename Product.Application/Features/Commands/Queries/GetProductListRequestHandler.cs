346b0cd38a096f48ee90b1bf9ca6abc787740282
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 10:22:23 +0300

    Add a request to get a list of products

---
+++ Product.Application/Features/Commands/Queries/GetProductListRequestHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Product.Application.DTOs.Products;
+using Product.Application.Features.Requests.Queries;
+using Product.Application.Interfaces;
+
+namespace Product.Application.Features.Commands.Queries
+{
+    public class GetProductListRequestHandler(IProductDbContext context, IMapper mapper) : IRequestHandler<GetProductListRequest, IReadOnlyCollection<ProductDto>>
+    {
+        private readonly IProductDbContext _context = context;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<IReadOnlyCollection<ProductDto>> Handle(GetProductListRequest request, CancellationToken cancellationToken)
+        {
+            var products = await _context.Products.ProjectTo<ProductDto>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+            return _mapper.Map<IReadOnlyCollection<ProductDto>>(products);
+        }
+    }
+}
---
+++ Product.Application/Features/Requests/Queries/GetProductListRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Product.Application.DTOs.Products;
+
+namespace Product.Application.Features.Requests.Queries
+{
+    public class GetProductListRequest : IRequest<IReadOnlyCollection<ProductDto>>
+    {
+
+    }
+}
