8a08d1394ea2d611a556fb2ae86692b7de5814ab
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-20 13:45:28 +0300

    Add a request for product details

---
+++ Product.Application/Features/Commands/Queries/GetProductDetailsRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Product.Application.DTOs.Products;
+using Product.Application.Features.Requests.Queries;
+using Product.Application.Interfaces;
+
+namespace Product.Application.Features.Commands.Queries
+{
+    public class GetProductDetailsRequestHandler(IProductDbContext context, IMapper mapper) : IRequestHandler<GetProductDetailsRequest, ProductDto>
+    {
+        private readonly IProductDbContext _context = context;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<ProductDto> Handle(GetProductDetailsRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(key => key.Id == request.Id, cancellationToken);
+            return _mapper.Map<ProductDto>(product);
+        }
+    }
+}
---
+++ Product.Application/Features/Requests/Queries/GetProductDetailsRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Product.Application.DTOs.Products;
+
+namespace Product.Application.Features.Requests.Queries
+{
+    public class GetProductDetailsRequest : IRequest<ProductDto>
+    {
+        public int Id { get; set; }
+    }
+}
