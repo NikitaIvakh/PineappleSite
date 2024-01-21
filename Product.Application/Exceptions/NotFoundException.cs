b7f933868aa0665d715e68b228562ff812861df7
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-21 10:40:04 +0300

    Create NotFoundException

---
+++ Product.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Product.Application.Exceptions
+{
+    public class NotFoundException(string message, object key) : ApplicationException($"{message} ({key}) не найдено")
+    {
+
+    }
+}
