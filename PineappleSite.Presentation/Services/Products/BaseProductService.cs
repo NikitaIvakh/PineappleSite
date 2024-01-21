e18cc024f7841b718eb6a8558b916ba061cbdbac
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-21 13:58:52 +0300

    Create BaseProductService service and ProductAPIViewModel class

---
+++ PineappleSite.Presentation/Services/Products/BaseProductService.cs
@@ -0,0 +1,37 @@
+﻿using PineappleSite.Presentation.Contracts;
+using System.Net.Http.Headers;
+
+namespace PineappleSite.Presentation.Services.Products
+{
+    public class BaseProductService(ILocalStorageService localStorageService, IProductClient productClient)
+    {
+        private readonly ILocalStorageService _localStorageService = localStorageService;
+        private readonly IProductClient _productClient = productClient;
+
+        protected ProductAPIViewModel ConvertProductException(ProductExceptions productExceptions)
+        {
+            if (productExceptions.StatusCode == 400)
+            {
+                return new ProductAPIViewModel() { Message = "Произошли ошибки валидации.", ValidationErrors = productExceptions.Response, IsSuccess = false };
+            }
+
+            else if (productExceptions.StatusCode == 404)
+            {
+                return new ProductAPIViewModel() { Message = "Требуемый элемент не удалось найти.", IsSuccess = false };
+            }
+
+            else
+            {
+                return new ProductAPIViewModel() { Message = "Что-то пошло не так, пожалуйста, попробуйте еще раз.", IsSuccess = false };
+            }
+        }
+
+        protected void AddBearerToken()
+        {
+            if (_localStorageService.Exists("token"))
+            {
+                _productClient.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _localStorageService.GetStorageValue<string>("token"));
+            }
+        }
+    }
+}
---
+++ PineappleSite.Presentation/Services/Products/ProductAPIViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PineappleSite.Presentation.Services.Products
+{
+    public class ProductAPIViewModel
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsSuccess { get; set; }
+
+        public string ValidationErrors { get; set; }
+    }
+}
