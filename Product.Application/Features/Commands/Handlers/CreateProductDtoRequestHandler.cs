1cbb5e4cd0d476615f1f4f99df5d994bf714f2e3
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-21 09:49:37 +0300

    Update the product creation command

--- Product.Application/Features/Commands/Handlers/CreateProductDtoRequestHandler.cs
+++ Product.Application/Features/Commands/Handlers/CreateProductDtoRequestHandler.cs
@@ -1,24 +1,55 @@
 ﻿using AutoMapper;
 using MediatR;
+using Product.Application.DTOs.Validator;
 using Product.Application.Features.Requests.Handlers;
 using Product.Application.Interfaces;
+using Product.Application.Response;
 using Product.Core.Entities.Producrs;
 
 namespace Product.Application.Features.Commands.Handlers
 {
-    public class CreateProductDtoRequestHandler(IProductDbContext context, IMapper mapper) : IRequestHandler<CreateProductDtoRequest, int>
+    public class CreateProductDtoRequestHandler(IProductDbContext context, IMapper mapper, ICreateProductDtoValidator createValidator) : IRequestHandler<CreateProductDtoRequest, ProductAPIResponse>
     {
         private readonly IProductDbContext _context = context;
         private readonly IMapper _mapper = mapper;
+        private readonly ICreateProductDtoValidator _createValidator = createValidator;
+        private readonly ProductAPIResponse _productAPIResponse = new ProductAPIResponse();
 
-        public async Task<int> Handle(CreateProductDtoRequest request, CancellationToken cancellationToken)
+        public async Task<ProductAPIResponse> Handle(CreateProductDtoRequest request, CancellationToken cancellationToken)
         {
-            var product = _mapper.Map<ProductEntity>(request.CreateProduct);
+            try
+            {
+                var validator = await _createValidator.ValidateAsync(request.CreateProduct, cancellationToken);
 
-            await _context.Products.AddAsync(product, cancellationToken);
-            await _context.SaveChangesAsync(cancellationToken);
+                if (!validator.IsValid)
+                {
+                    _productAPIResponse.IsSuccess = false;
+                    _productAPIResponse.Message = "Ошибка создания продукта";
+                    _productAPIResponse.ValidationErrors = validator.Errors.Select(e => e.ErrorMessage).ToList();
+                }
 
-            return product.Id;
+                else
+                {
+                    var product = _mapper.Map<ProductEntity>(request.CreateProduct);
+
+                    await _context.Products.AddAsync(product, cancellationToken);
+                    await _context.SaveChangesAsync(cancellationToken);
+
+                    _productAPIResponse.IsSuccess = true;
+                    _productAPIResponse.Message = "Продукт успешно добавлен";
+                    _productAPIResponse.Id = product.Id;
+
+                    return _productAPIResponse;
+                }
+            }
+
+            catch (Exception exception)
+            {
+                _productAPIResponse.IsSuccess = false;
+                _productAPIResponse.Message = exception.Message;
+            }
+
+            return _productAPIResponse;
         }
     }
 }
--- Product.Application/Features/Requests/Handlers/CreateProductDtoRequest.cs
+++ Product.Application/Features/Requests/Handlers/CreateProductDtoRequest.cs
@@ -1,9 +1,10 @@
 ﻿using MediatR;
 using Product.Application.DTOs.Products;
+using Product.Application.Response;
 
 namespace Product.Application.Features.Requests.Handlers
 {
-    public class CreateProductDtoRequest : IRequest<int>
+    public class CreateProductDtoRequest : IRequest<ProductAPIResponse>
     {
         public CreateProductDto CreateProduct { get; set; }
     }
