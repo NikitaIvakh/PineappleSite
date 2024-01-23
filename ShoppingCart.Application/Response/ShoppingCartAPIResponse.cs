5e54b7a402a2899dde5830271538ef908b3e521a
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 11:52:44 +0300

    Add ShoppingCartAPIResponse class

---
+++ ShoppingCart.Application/Response/ShoppingCartAPIResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingCart.Application.Response
+{
+    public class ShoppingCartAPIResponse
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsSuccess { get; set; } = true;
+
+        public List<string> ValidationErrors { get; set; }
+    }
+}
