ead7c8847602dcd36f1f00b062515558327bfb3b
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 10:52:21 +0300

    Create ICartHeaderDbContext interface

---
+++ ShoppingCart.Application/Interfaces/ICartHeaderDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Core.Entities.Cart;
+
+namespace ShoppingCart.Application.Interfaces
+{
+    public interface ICartHeaderDbContext
+    {
+        public DbSet<CartHeader> CartHeaders { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
