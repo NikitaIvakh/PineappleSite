91208aae30afb5118d19c04df8827fd33aa0560b
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 11:45:53 +0300

    Register AutoMapper

---
+++ ShoppingCart.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ShoppingCart.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection ConfigureApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
