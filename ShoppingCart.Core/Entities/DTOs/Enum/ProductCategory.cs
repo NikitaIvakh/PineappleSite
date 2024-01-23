12be3c0dbddc7c6f970797193593cc0236ab567b
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 10:48:23 +0300

    Create CartDetails class

---
+++ ShoppingCart.Core/Entities/DTOs/Enum/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.Core.Entities.DTOs.Enum
+{
+    public enum ProductCategory
+    {
+        Soups = 1,
+        Snacks = 2,
+        Drinks = 3,
+    }
+}
---
+++ ShoppingCart.Core/Entities/DTOs/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingCart.Core.Entities.DTOs.Enum;
+
+namespace ShoppingCart.Core.Entities.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+
+        public double Price { get; set; }
+    }
+}
