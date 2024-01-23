55c4f6a70222610cf48e474d48b6742e7794f6b0
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 15:58:38 +0300

    Add a class for testing queries

---
+++ ShoppingCart.Test/Common/TestQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using ShoppingCart.Application.Profiles;
+using ShoppingCart.Infrastructure;
+using Xunit;
+
+namespace ShoppingCart.Test.Common
+{
+    public class TestQueryHandler : IDisposable
+    {
+        protected ShoppingCartDbContext Context;
+        protected IMapper Mapper;
+
+        public TestQueryHandler()
+        {
+            Context = ShoppingCartDbContextFactory.Create();
+
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            ShoppingCartDbContextFactory.Desctroy(Context);
+        }
+    }
+
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<TestQueryHandler> { }
+}
