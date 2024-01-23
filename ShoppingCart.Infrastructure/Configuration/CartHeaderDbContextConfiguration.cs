ab76f2e6e27f7af4444f6e969668e1e430659444
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 11:13:48 +0300

    Add migration. Add entity configuration

---
+++ ShoppingCart.Infrastructure/Configuration/CartHeaderDbContextConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoppingCart.Core.Entities.Cart;
+
+namespace ShoppingCart.Infrastructure.Configuration
+{
+    public class CartHeaderDbContextConfiguration : IEntityTypeConfiguration<CartHeader>
+    {
+        public void Configure(EntityTypeBuilder<CartHeader> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(key => key.UserId).IsRequired(false);
+            builder.Property(key => key.CouponCode).IsRequired(false);
+        }
+    }
+}
---
+++ ShoppingCart.Infrastructure/Configuration/CratDetailsDbContextConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoppingCart.Core.Entities.Cart;
+
+namespace ShoppingCart.Infrastructure.Configuration
+{
+    public class CratDetailsDbContextConfiguration : IEntityTypeConfiguration<CartDetails>
+    {
+        public void Configure(EntityTypeBuilder<CartDetails> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Ignore(key => key.Product);
+        }
+    }
+}
