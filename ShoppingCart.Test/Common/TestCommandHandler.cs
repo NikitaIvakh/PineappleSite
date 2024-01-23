e1712a1af16a0152df8802a4db63186288a6856a
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 16:49:58 +0300

    Add a class for testing commands

---
+++ ShoppingCart.Test/Common/TestCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using ShoppingCart.Application.Profiles;
+using ShoppingCart.Infrastructure;
+
+namespace ShoppingCart.Test.Common
+{
+    public class TestCommandHandler : IDisposable
+    {
+        protected ShoppingCartDbContext Context;
+        protected IMapper Mapper;
+
+        public TestCommandHandler()
+        {
+            Context = ShoppingCartDbContextFactory.Create();
+
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            ShoppingCartDbContextFactory.Desctroy(Context);
+        }
+    }
+}
