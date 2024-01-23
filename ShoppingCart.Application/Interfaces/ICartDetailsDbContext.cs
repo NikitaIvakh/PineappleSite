3b79dfe5a7e2963060f335f6f216e09f595c7d75
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 10:57:40 +0300

    Create database context

---
+++ ShoppingCart.Application/Interfaces/ICartDetailsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Core.Entities.Cart;
+
+namespace ShoppingCart.Application.Interfaces
+{
+    public interface ICartDetailsDbContext
+    {
+        DbSet<CartDetails> CartDetails { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
