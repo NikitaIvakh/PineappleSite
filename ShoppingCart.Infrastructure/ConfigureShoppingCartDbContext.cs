06c9d6ce043d82638bc791ed927563e14f767e14
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-23 11:01:32 +0300

    Create class for register db context

---
+++ ShoppingCart.Infrastructure/ConfigureShoppingCartDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingCart.Application.Interfaces;
+
+namespace ShoppingCart.Infrastructure
+{
+    public static class ConfigureShoppingCartDbContext
+    {
+        public static IServiceCollection ConfigureShoppingCartService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ShoppingCartDbContext>(config =>
+            {
+                config.UseNpgsql(configuration.GetConnectionString("ShoppingCartDbConnectionString"));
+            });
+
+            services.AddScoped<ICartHeaderDbContext, ShoppingCartDbContext>();
+            services.AddScoped<ICartDetailsDbContext, ShoppingCartDbContext>();
+
+            return services;
+        }
+    }
+}
