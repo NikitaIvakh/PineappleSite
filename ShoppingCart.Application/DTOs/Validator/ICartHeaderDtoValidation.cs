15883131056020cb0ab56c6548b3f7286fb5bb37
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-24 18:24:24 +0300

    Add validator

---
+++ ShoppingCart.Application/DTOs/Validator/ICartHeaderDtoValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ShoppingCart.Application.DTOs.Cart;
+
+namespace ShoppingCart.Application.DTOs.Validator
+{
+    public class ICartHeaderDtoValidation : AbstractValidator<CartHeaderDto>
+    {
+        public ICartHeaderDtoValidation()
+        {
+            RuleFor(key => key.Id).NotEmpty().NotNull();
+            RuleFor(key => key.UserId).NotEmpty().NotNull();
+            RuleFor(key => key.CouponCode).MaximumLength(20).WithMessage("Длина строки не может превышать 20 символов");
+            RuleFor(key => key.Discount).GreaterThanOrEqualTo(1).WithMessage("Сумма скидки не может быть меньше 1");
+        }
+    }
+}
