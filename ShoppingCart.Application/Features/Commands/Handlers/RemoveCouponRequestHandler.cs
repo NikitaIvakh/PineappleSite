fe4ce5ecf314c7124cc35c8450c43e9eed3e9b1c
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-24 11:29:06 +0300

    Create command for remove coupon

---
+++ ShoppingCart.Application/Features/Commands/Handlers/RemoveCouponRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Application.Features.Requests.Handlers;
+using ShoppingCart.Application.Interfaces;
+using ShoppingCart.Application.Response;
+using ShoppingCart.Core.Entities.Cart;
+
+namespace ShoppingCart.Application.Features.Commands.Handlers
+{
+    public class RemoveCouponRequestHandler : IRequestHandler<RemoveCouponRequest, ShoppingCartAPIResponse>
+    {
+        private readonly ICartHeaderDbContext _cartHeaderContext;
+        private readonly IMapper _mapper;
+        private readonly ShoppingCartAPIResponse _response = new();
+
+        public async Task<ShoppingCartAPIResponse> Handle(RemoveCouponRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                CartHeader cartHeaderFromDb = await _cartHeaderContext.CartHeaders.FirstAsync(key => key.UserId == request.CartDto.CartHeader.UserId, cancellationToken);
+                cartHeaderFromDb.CouponCode = string.Empty;
+                _cartHeaderContext.CartHeaders.Update(cartHeaderFromDb);
+                await _cartHeaderContext.SaveChangesAsync(cancellationToken);
+            }
+
+            catch (Exception exception)
+            {
+                _response.IsSuccess = false;
+                _response.Message = exception.Message;
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ ShoppingCart.Application/Features/Requests/Handlers/RemoveCouponRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using ShoppingCart.Application.DTOs.Cart;
+using ShoppingCart.Application.Response;
+
+namespace ShoppingCart.Application.Features.Requests.Handlers
+{
+    public class RemoveCouponRequest : IRequest<ShoppingCartAPIResponse>
+    {
+        public CartDto CartDto { get; set; }
+    }
+}
