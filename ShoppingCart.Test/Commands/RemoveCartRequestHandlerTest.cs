2ed338944e734ee454d7f3823171493730b22776
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-24 13:18:45 +0300

    Create test for remove cart details

---
+++ ShoppingCart.Test/Commands/RemoveCartRequestHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿using ShoppingCart.Application.Features.Commands.Handlers;
+using ShoppingCart.Application.Features.Requests.Handlers;
+using ShoppingCart.Application.Response;
+using ShoppingCart.Test.Common;
+using Shouldly;
+using Xunit;
+
+namespace ShoppingCart.Test.Commands
+{
+    public class RemoveCartRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task RemoveCartRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new RemoveCartRequestHandlerc(Context, Context, Mapper);
+            var cartDetailsId = 1;
+
+            // Act
+            var result = await handler.Handle(new RemoveCartRequest
+            {
+                CartDetailsId = cartDetailsId,
+            }, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.ShouldBeTrue();
+            result.ShouldBeOfType<ShoppingCartAPIResponse>();
+        }
+    }
+}
