4cbe1c131da0c4acbbb45aa240ed5a56ad216fd1
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-24 10:49:25 +0300

    Create tests

--- Coupon.Application/Features/Coupons/Handlers/Queries/GetCouponDetailsByCouponNameRequestHandler.cs
+++ Coupon.Application/Features/Coupons/Handlers/Queries/GetCouponDetailsByCouponNameRequestHandler.cs
@@ -3,6 +3,7 @@ using Coupon.Application.DTOs;
 using Coupon.Application.Exceptions;
 using Coupon.Application.Features.Coupons.Requests.Queries;
 using Coupon.Application.Interfaces;
+using Coupon.Core.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,7 @@ namespace Coupon.Application.Features.Coupons.Handlers.Queries
         public async Task<CouponDto> Handle(GetCouponDetailsByCouponNameRequest request, CancellationToken cancellationToken)
         {
             var coupon = await _context.Coupons.FirstOrDefaultAsync(key => key.CouponCode.ToLower() == request.CouponCode.ToLower(), cancellationToken) ??
-                throw new NotFoundException($"Купон с названием: ", request.CouponCode);
+                throw new NotFoundException(nameof(CouponEntity), request.CouponCode);
 
             return _mapper.Map<CouponDto>(coupon);
         }
