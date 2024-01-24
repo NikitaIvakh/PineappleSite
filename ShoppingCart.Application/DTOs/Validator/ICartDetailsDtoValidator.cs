0e76fd0551a7122298439e872882400495c07940
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-24 18:25:58 +0300

    Add validator

---
+++ ShoppingCart.Application/DTOs/Validator/ICartDetailsDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using ShoppingCart.Application.DTOs.Cart;
+
+namespace ShoppingCart.Application.DTOs.Validator
+{
+    public class ICartDetailsDtoValidator : AbstractValidator<CartDetailsDto>
+    {
+        public ICartDetailsDtoValidator()
+        {
+            RuleFor(key => key.Id).NotNull().NotEmpty();
+        }
+    }
+}
