4e55683d34983fce5d177c26f78310ce5cd53cc2
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-25 13:09:26 +0300

    Update query for get cart

--- ShoppingCart.Application/Features/Commands/Queries/GetShoppingCartRequestHandler.cs
+++ ShoppingCart.Application/Features/Commands/Queries/GetShoppingCartRequestHandler.cs
@@ -21,46 +21,57 @@ namespace ShoppingCart.Application.Features.Commands.Queries
 
         public async Task<ShoppingCartAPIResponse> Handle(GetShoppingCartRequest request, CancellationToken cancellationToken)
         {
-            var cartHeader = await _cartContext.CartHeaders.FirstOrDefaultAsync(key => key.UserId == request.UserId, cancellationToken);
-
-            if (cartHeader is null)
+            try
             {
+                var cartHeader = await _cartContext.CartHeaders.FirstOrDefaultAsync(key => key.UserId == request.UserId, cancellationToken);
+
+                if (cartHeader is null)
+                {
+                    _shoppingCartAPIResponse.IsSuccess = true;
+                    _shoppingCartAPIResponse.Data = new CartDto();
+
+                    return _shoppingCartAPIResponse;
+                }
+
+                var cartDetails = await _detailsContext.CartDetails.Where(key => key.CartHeaderId == cartHeader.Id).ToListAsync(cancellationToken);
+
+                CartDto cartDto = new()
+                {
+                    CartHeader = _mapper.Map<CartHeaderDto>(cartHeader),
+                    CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(cartDetails),
+                };
+
+                IEnumerable<ProductDto> productDtos = await _productService.GetProductsAsync();
+
+                foreach (var item in cartDto.CartDetails)
+                {
+                    item.Product = productDtos.FirstOrDefault(key => key.Id == item.ProductId);
+                    cartDto.CartHeader.CartTotal += (item.Count * item.Product.Price);
+                }
+
+                if (!string.IsNullOrEmpty(cartDto.CartHeader.CouponCode))
+                {
+                    var coupon = await _couponService.GetCouponAsync(cartDto.CartHeader.CouponCode);
+
+                    if (coupon is not null && cartDto.CartHeader.CartTotal > coupon.MinAmount)
+                    {
+                        cartDto.CartHeader.CartTotal -= coupon.DiscountAmount;
+                        cartDto.CartHeader.Discount = coupon.DiscountAmount;
+                    }
+                }
+
                 _shoppingCartAPIResponse.IsSuccess = true;
-                _shoppingCartAPIResponse.Data = new CartDto();
+                _shoppingCartAPIResponse.Message = "Купон успешно применен";
+                _shoppingCartAPIResponse.Data = cartDto;
 
                 return _shoppingCartAPIResponse;
             }
 
-            var cartDetails = await _detailsContext.CartDetails.Where(key => key.CartHeaderId == cartHeader.Id).ToListAsync(cancellationToken);
-
-            CartDto cartDto = new()
+            catch (Exception exception)
             {
-                CartHeader = _mapper.Map<CartHeaderDto>(cartHeader),
-                CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(cartDetails),
-            };
-
-            IEnumerable<ProductDto> productDtos = await _productService.GetProductsAsync();
-
-            foreach (var item in cartDto.CartDetails)
-            {
-                item.Product = productDtos.FirstOrDefault(key => key.Id == item.ProductId);
-                cartDto.CartHeader.CartTotal += (item.Count * item.Product.Price);
+                _shoppingCartAPIResponse.IsSuccess = false;
+                _shoppingCartAPIResponse.Message = exception.Message;
             }
-
-            if (!string.IsNullOrEmpty(cartDto.CartHeader.CouponCode))
-            {
-                var coupon = await _couponService.GetCouponAsync(cartDto.CartHeader.CouponCode);
-
-                if (coupon is not null && cartDto.CartHeader.CartTotal > coupon.MinAmount)
-                {
-                    cartDto.CartHeader.CartTotal -= coupon.DiscountAmount;
-                    cartDto.CartHeader.Discount = coupon.DiscountAmount;
-                }
-            }
-
-            _shoppingCartAPIResponse.IsSuccess = true;
-            _shoppingCartAPIResponse.Message = "Купон успешно применен";
-            _shoppingCartAPIResponse.Data = cartDto;
 
             return _shoppingCartAPIResponse;
         }
