381c3c8f46a0f527b836eafc1bbb0d3ea9f0ff75
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-25 10:22:56 +0300

    Register service

--- PineappleSite.Presentation/Program.cs
+++ PineappleSite.Presentation/Program.cs
@@ -25,6 +25,7 @@ applicationBuilder.Services.AddSingleton<ILocalStorageService, LocalStorageServi
 applicationBuilder.Services.AddScoped<ICouponService, CouponService>();
 applicationBuilder.Services.AddScoped<IUserService, UserService>();
 applicationBuilder.Services.AddScoped<IProductService, ProductService>();
+applicationBuilder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
 
 applicationBuilder.Services.Configure<CookiePolicyOptions>(options =>
 {
