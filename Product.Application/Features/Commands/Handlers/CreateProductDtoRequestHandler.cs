a71d6a44905331fc57593c52a014dcf64f8dfade
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-25 16:37:22 +0300

    Add migration. Add work with image

--- Product.Application/Features/Commands/Handlers/CreateProductDtoRequestHandler.cs
+++ Product.Application/Features/Commands/Handlers/CreateProductDtoRequestHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Product.Application.DTOs.Validator;
 using Product.Application.Features.Requests.Handlers;
 using Product.Application.Interfaces;
@@ -8,12 +9,13 @@ using Product.Core.Entities.Producrs;
 
 namespace Product.Application.Features.Commands.Handlers
 {
-    public class CreateProductDtoRequestHandler(IProductDbContext context, IMapper mapper, ICreateProductDtoValidator createValidator) : IRequestHandler<CreateProductDtoRequest, ProductAPIResponse>
+    public class CreateProductDtoRequestHandler(IProductDbContext context, IMapper mapper, ICreateProductDtoValidator createValidator, IHttpContextAccessor httpContextAccessor) : IRequestHandler<CreateProductDtoRequest, ProductAPIResponse>
     {
         private readonly IProductDbContext _context = context;
         private readonly IMapper _mapper = mapper;
         private readonly ICreateProductDtoValidator _createValidator = createValidator;
-        private readonly ProductAPIResponse _productAPIResponse = new ProductAPIResponse();
+        private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+        private readonly ProductAPIResponse _productAPIResponse = new();
 
         public async Task<ProductAPIResponse> Handle(CreateProductDtoRequest request, CancellationToken cancellationToken)
         {
@@ -34,6 +36,33 @@ namespace Product.Application.Features.Commands.Handlers
 
                     await _context.Products.AddAsync(product, cancellationToken);
                     await _context.SaveChangesAsync(cancellationToken);
+
+                    if (request.CreateProduct.Avatar is not null)
+                    {
+                        string fileName = product.Id + Path.GetExtension(request.CreateProduct.Avatar.FileName);
+                        string filePath = Path.Combine("wwwroot", "ProducImages", fileName);
+                        var directoryLocation = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+
+                        FileInfo fileInfo = new(directoryLocation);
+
+                        if (fileInfo.Exists)
+                            fileInfo.Delete();
+
+                        var fileDirectory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+                        using (FileStream fileStream = new(fileDirectory, FileMode.Create))
+                        {
+                            request.CreateProduct.Avatar.CopyTo(fileStream);
+                        }
+
+                        var baseUrl = $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host.Value}{_httpContextAccessor.HttpContext.Request.PathBase.Value}";
+                        product.ImageUrl = Path.Combine(baseUrl, "ProducImages", fileName);
+                        product.ImageLocalPath = filePath;
+                    }
+
+                    else
+                    {
+                        product.ImageUrl = "https://placehold.co/600x400";
+                    }
 
                     _productAPIResponse.IsSuccess = true;
                     _productAPIResponse.Message = "Продукт успешно добавлен";
---
+++ Product.Infrastructure/Migrations/20240125131155_AddNewFields.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageLocalPath",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ImageLocalPath", "ImageUrl" },
+                values: new object[] { null, null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "ImageLocalPath", "ImageUrl" },
+                values: new object[] { null, null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "ImageLocalPath", "ImageUrl" },
+                values: new object[] { null, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageLocalPath",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+        }
+    }
+}
