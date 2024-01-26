62684467ff5e2e340751f0bdec37d8750a62656b
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 11:04:30 +0300

    Add new entity, update mapping

---
+++ Favourites.Application/DTOs/ProductDto.cs
@@ -0,0 +1,21 @@
+﻿using Favourites.Application.DTOs.Enum;
+
+namespace Favourites.Application.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ProductCategory ProductCategory { get; set; }
+
+        public double Price { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? ImageLocalPath { get; set; }
+    }
+}
---
+++ Favourites.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Favourites.Application.DTOs;
+using Favourites.Domain.Entities.Favourite;
+
+namespace Favourites.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<FavouritesHeader, FavoutiteHeaderDto>().ReverseMap();
+            CreateMap<FavouritesDetails, FavouritesDetailsDto>().ReverseMap();
+            CreateMap<Domain.Entities.DTOs.ProductDto, ProductDto>().ReverseMap();
+        }
+    }
+}
