6055ba9e5775c73a1460b269d0ec9f9d18405548
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 11:19:33 +0300

    Create FavouriteAPIResponse class

---
+++ Favourites.Application/Response/FavouriteAPIResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Favourites.Application.Response
+{
+    public class FavouriteAPIResponse
+    {
+        public int Id { get; set; }
+
+        public object? Data { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public bool IsSuccess { get; set; } = true;
+
+        public List<string> ValidationErrors { get; set; }
+    }
+}
