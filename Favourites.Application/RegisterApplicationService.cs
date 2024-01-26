a652dafb99b6115bc0a61219869c02faa41b88db
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 11:06:18 +0300

    Register automapper

---
+++ Favourites.Application/RegisterApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Favourites.Application
+{
+    public static class RegisterApplicationService
+    {
+        public static IServiceCollection ConfigureApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
