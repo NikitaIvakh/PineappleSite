ca50ca79e0c7ff336da61499892d025cbc809a01
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 14:55:31 +0300

    Add a product removal command

---
+++ Favourites.Application/Features/Commands/Handlers/RemoveFavoriteRequestHandler.cs
@@ -0,0 +1,49 @@
+﻿using Favourites.Application.Features.Requests.Handlers;
+using Favourites.Application.Interfaces;
+using Favourites.Application.Response;
+using Favourites.Domain.Entities.Favourite;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Favourites.Application.Features.Commands.Handlers
+{
+    public class RemoveFavoriteRequestHandler(IFavoutiteHeaderDbContext favoutiteHeaderDbContext, IFavoutiteDetailsDbContext favoutiteDetailsDbContext) : IRequestHandler<RemoveFavoriteRequest, FavouriteAPIResponse>
+    {
+        private readonly IFavoutiteHeaderDbContext _favoutiteHeaderDbContext = favoutiteHeaderDbContext;
+        private readonly IFavoutiteDetailsDbContext _favoutiteDetailsDbContext = favoutiteDetailsDbContext;
+        private readonly FavouriteAPIResponse _response = new();
+
+        public async Task<FavouriteAPIResponse> Handle(RemoveFavoriteRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                FavouritesDetails favouritesDetails = await _favoutiteDetailsDbContext.FavouritesDetails.FirstOrDefaultAsync(key => key.FavouritesDetailsId == request.FavouriteDetailId, cancellationToken);
+                int removeProduct = _favoutiteDetailsDbContext.FavouritesDetails.Where(key => key.FavouritesHeaderId == favouritesDetails.FavouritesHeaderId).Count();
+                _favoutiteDetailsDbContext.FavouritesDetails.Remove(favouritesDetails);
+
+                if (removeProduct == 1)
+                {
+                    FavouritesHeader favouritesHeader = await _favoutiteHeaderDbContext.FavouritesHeaders.FirstOrDefaultAsync(key => key.FavouritesHeaderId == favouritesDetails.FavouritesHeaderId, cancellationToken);
+                    _favoutiteHeaderDbContext.FavouritesHeaders.Remove(favouritesHeader);
+                }
+
+                await _favoutiteHeaderDbContext.SaveChangesAsync(cancellationToken);
+                await _favoutiteDetailsDbContext.SaveChangesAsync(cancellationToken);
+
+                _response.IsSuccess = true;
+                _response.Id = request.FavouriteDetailId;
+                _response.Data = request.FavouriteDetailId;
+
+                return _response;
+            }
+
+            catch (Exception exception)
+            {
+                _response.IsSuccess = false;
+                _response.Message = exception.Message;
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ Favourites.Application/Features/Requests/Handlers/RemoveFavoriteRequest.cs
@@ -0,0 +1,11 @@
+﻿using Favourites.Application.DTOs;
+using Favourites.Application.Response;
+using MediatR;
+
+namespace Favourites.Application.Features.Requests.Handlers
+{
+    public class RemoveFavoriteRequest : IRequest<FavouriteAPIResponse>
+    {
+        public int FavouriteDetailId { get; set; }
+    }
+}
