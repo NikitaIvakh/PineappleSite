129b8dd54832d97cb9f8cda2d5201518e3e35219
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 10:03:33 +0300

    Create FavouritesHeader class

---
+++ Favourites.Domain/Entities/Favourite/FavouritesHeader.cs
@@ -0,0 +1,9 @@
+﻿namespace Favourites.Domain.Entities.Favourite
+{
+    public class FavouritesHeader
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
