44d8ce798b42d6227be5bf054658c3fb7ef1cc43
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 10:14:53 +0300

    Create IFavoutiteDetailsDbContext interface

---
+++ Favourites.Application/Interfaces/IFavoutiteDetailsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Favourites.Domain.Entities.Favourite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Favourites.Application.Interfaces
+{
+    public interface IFavoutiteDetailsDbContext
+    {
+        DbSet<FavouritesDetails> FavouritesDetails { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
