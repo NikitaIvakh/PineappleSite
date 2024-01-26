2d0aae1e88abca3cbf819cdfefb3869347d0e8b9
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 10:13:35 +0300

    Create IFavoutiteHeaderDbContext interface

---
+++ Favourites.Application/Interfaces/IFavoutiteHeaderDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Favourites.Domain.Entities.Favourite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Favourites.Application.Interfaces
+{
+    public interface IFavoutiteHeaderDbContext
+    {
+        DbSet<FavouritesHeader> FavouritesHeaders { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
