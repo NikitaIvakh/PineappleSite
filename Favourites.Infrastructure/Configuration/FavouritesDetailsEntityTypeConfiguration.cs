f1fae4c1f86a41f4708ce1180aedbd39b20b1eae
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-26 10:39:10 +0300

    Create configuration

---
+++ Favourites.Infrastructure/Configuration/FavouritesDetailsEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Favourites.Domain.Entities.Favourite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Favourites.Infrastructure.Configuration
+{
+    public class FavouritesDetailsEntityTypeConfiguration : IEntityTypeConfiguration<FavouritesDetails>
+    {
+        public void Configure(EntityTypeBuilder<FavouritesDetails> builder)
+        {
+            builder.HasKey(key => key.FavouritesDetailsId);
+            builder.HasOne(key => key.FavouritesHeader).WithMany().HasForeignKey(key => key.FavouritesHeaderId);
+
+            builder.Ignore(key => key.Product);
+        }
+    }
+}
---
+++ Favourites.Infrastructure/Configuration/FavoutiteHeaderEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Favourites.Domain.Entities.Favourite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Favourites.Infrastructure.Configuration
+{
+    public class FavoutiteHeaderEntityTypeConfiguration : IEntityTypeConfiguration<FavouritesHeader>
+    {
+        public void Configure(EntityTypeBuilder<FavouritesHeader> builder)
+        {
+            builder.HasKey(key => key.FavouritesHeaderId);
+            builder.Property(key => key.UserId).IsRequired(false);
+        }
+    }
+}
