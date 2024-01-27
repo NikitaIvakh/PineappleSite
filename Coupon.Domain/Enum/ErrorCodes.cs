a249f2c641e3a16a0617b72ad5ce244ec3f1c183
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-27 09:35:42 +0300

    Create enum error codes

---
+++ Coupon.Domain/Enum/ErrorCodes.cs
@@ -0,0 +1,19 @@
+﻿namespace Coupon.Domain.Enum
+{
+    public enum ErrorCodes
+    {
+        CouponNotFound1 = 1,
+
+        CouponNotFound2 = 2,
+
+        CouponsNotFound = 3,
+
+        CouponAlreadyExists = 4,
+
+        CouponNotCreated = 5,
+
+        CouponNotDeleted = 6,
+
+        InternalServerError = 7,
+    }
+}
