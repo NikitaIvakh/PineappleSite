f7f95b26e1f4633582ffe64fdc1a2cfc19ecda58
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-28 13:29:21 +0300

    Add Serilog

--- Coupon.API/Program.cs
+++ Coupon.API/Program.cs
@@ -1,61 +1,39 @@
 using Coupon.Application.DependencyInjection;
 using Coupon.Infrastructure.DependencyInjection;
 using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
-namespace Coupon.API
+WebApplicationBuilder applicationBuilder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+applicationBuilder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+applicationBuilder.Services.AddEndpointsApiExplorer();
+applicationBuilder.Services.AddSwaggerGen();
+
+applicationBuilder.Services.ConfigureApplicationService();
+applicationBuilder.Services.ConfigureInfrastructureServive(applicationBuilder.Configuration);
+applicationBuilder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration)
+    .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Warning))
+    .WriteTo.Logger(l => l.Filter.ByIncludingOnly(e => e.Level == LogEventLevel.Error))
+    .WriteTo.File(new JsonFormatter(), "./logs/debug-logs-.json", rollingInterval: RollingInterval.Day)
+    .WriteTo.File(new JsonFormatter(), "./logs/error-logs-.json", rollingInterval: RollingInterval.Day)
+    .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+);
+
+WebApplication webApplication = applicationBuilder.Build();
+
+// Configure the HTTP request pipeline.
+if (webApplication.Environment.IsDevelopment())
 {
-    internal class Program
-    {
-        private static async Task Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.Console()
-                .WriteTo.File("logs/CouponAPI.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-
-            Log.Information("Hello, world!");
-
-            try
-            {
-                WebApplicationBuilder applicationBuilder = WebApplication.CreateBuilder(args);
-
-                // Add services to the container.
-
-                applicationBuilder.Services.AddControllers();
-                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-                applicationBuilder.Services.AddEndpointsApiExplorer();
-                applicationBuilder.Services.AddSwaggerGen();
-
-                applicationBuilder.Services.ConfigureApplicationService();
-                applicationBuilder.Services.ConfigureInfrastructureServive(applicationBuilder.Configuration);
-                applicationBuilder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
-
-                WebApplication webApplication = applicationBuilder.Build();
-
-                // Configure the HTTP request pipeline.
-                if (webApplication.Environment.IsDevelopment())
-                {
-                    webApplication.UseSwagger();
-                    webApplication.UseSwaggerUI();
-                }
-
-                webApplication.UseHttpsRedirection();
-                webApplication.UseSerilogRequestLogging();
-                webApplication.UseAuthorization();
-                webApplication.MapControllers();
-                webApplication.Run();
-            }
-
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Something went wrong");
-            }
-
-            finally
-            {
-                await Log.CloseAndFlushAsync();
-            }
-        }
-    }
+    webApplication.UseSwagger();
+    webApplication.UseSwaggerUI();
 }
+
+webApplication.UseHttpsRedirection();
+webApplication.UseSerilogRequestLogging();
+webApplication.UseAuthorization();
+webApplication.MapControllers();
+webApplication.Run();
