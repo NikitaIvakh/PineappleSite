81f54912e5c3b23d12c47cbcfe92ccfe59cde7f5
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-29 12:48:49 +0300

    Create class for test favourite products

---
+++ Favourites.Test/Common/FavouritesDbContextFactory.cs
@@ -0,0 +1,45 @@
+﻿using Favourites.Domain.DTOs;
+using Favourites.Domain.Interfaces.Repositories;
+using Favourites.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Favourites.Test.Common
+{
+    public static class FavouritesDbContextFactory
+    {
+        public static ApplicationDbContext Create(IBaseRepository<FavouritesDto> repository)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            var context = new ApplicationDbContext(options);
+            context.Database.EnsureCreated();
+            repository.CreateAsync(
+                new FavouritesDto()
+                {
+                    FavoutiteHeader = new FavoutiteHeaderDto
+                    {
+                        FavouritesHeaderId = 2,
+                        UserId = "testuserid2"
+                    },
+
+                    FavouritesDetails = new List<FavouritesDetailsDto>
+                    {
+                        new() {
+                            FavouritesDetailsId = 2,
+                            FavouritesHeaderId = 2,
+                            ProductId = 3,
+                        }
+                    }
+                }).GetAwaiter().GetResult();
+
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void DestroyDatabase(ApplicationDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
