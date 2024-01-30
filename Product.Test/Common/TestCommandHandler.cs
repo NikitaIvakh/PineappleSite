efadcded95b4ede506b691a983bfcbcc62b10877
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-30 19:09:41 +0300

    Update classes for test products

--- Product.Test/Common/TestCommandHandler.cs
+++ Product.Test/Common/TestCommandHandler.cs
@@ -1,44 +1,44 @@
-﻿//using AutoMapper;
-//using Microsoft.AspNetCore.Http;
-//using Moq;
-//using Product.Application.DTOs.Validator;
-//using Product.Application.Profiles;
-//using Product.Infrastructure;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Product.Application.DTOs.Validator;
+using Product.Application.Profiles;
+using Product.Infrastructure;
 
-//namespace Product.Test.Common
-//{
-//    public class TestCommandHandler : IDisposable
-//    {
-//        protected ApplicationDbContext Context;
-//        protected IMapper Mapper;
-//        protected ICreateProductDtoValidator CreateValidator;
-//        protected IUpdateProductDtoValidator UpdateValidator;
-//        protected IDeleteProductDtoValidator DeleteValidator;
-//        protected IDeleteProductsDtoValidator DeleteProductsValidator;
-//        protected IHttpContextAccessor HttpContextAccessor;
+namespace Product.Test.Common
+{
+    public class TestCommandHandler : IDisposable
+    {
+        protected ApplicationDbContext Context;
+        protected IMapper Mapper;
+        protected ICreateProductDtoValidator CreateValidator;
+        protected IUpdateProductDtoValidator UpdateValidator;
+        protected IDeleteProductDtoValidator DeleteValidator;
+        protected IDeleteProductsDtoValidator DeleteProductsValidator;
+        protected IHttpContextAccessor HttpContextAccessor;
 
-//        public TestCommandHandler()
-//        {
-//            var httpContextAccessor = new Mock<IHttpContextAccessor>();
-//            HttpContextAccessor = httpContextAccessor.Object;
+        public TestCommandHandler()
+        {
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            HttpContextAccessor = httpContextAccessor.Object;
 
-//            Context = ProductDbContextFactory.Create();
-//            CreateValidator = new ICreateProductDtoValidator();
-//            UpdateValidator = new IUpdateProductDtoValidator();
-//            DeleteValidator = new IDeleteProductDtoValidator();
-//            DeleteProductsValidator = new IDeleteProductsDtoValidator();
+            Context = ProductDbContextFactory.Create();
+            CreateValidator = new ICreateProductDtoValidator();
+            UpdateValidator = new IUpdateProductDtoValidator();
+            DeleteValidator = new IDeleteProductDtoValidator();
+            DeleteProductsValidator = new IDeleteProductsDtoValidator();
 
-//            var mapperConfiguration = new MapperConfiguration(config =>
-//            {
-//                config.AddProfile<MappingProfile>();
-//            });
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
 
-//            Mapper = mapperConfiguration.CreateMapper();
-//        }
+            Mapper = mapperConfiguration.CreateMapper();
+        }
 
-//        public void Dispose()
-//        {
-//            ProductDbContextFactory.Destroy(Context);
-//        }
-//    }
-//}
+        public void Dispose()
+        {
+            ProductDbContextFactory.Destroy(Context);
+        }
+    }
+}
--- Product.Test/Common/TestQueryHandler.cs
+++ Product.Test/Common/TestQueryHandler.cs
@@ -1,33 +1,33 @@
-﻿//using AutoMapper;
-//using Product.Application.Profiles;
-//using Product.Infrastructure;
-//using Xunit;
+﻿using AutoMapper;
+using Product.Application.Profiles;
+using Product.Infrastructure;
+using Xunit;
 
-//namespace Product.Test.Common
-//{
-//    public class TestQueryHandler : IDisposable
-//    {
-//        protected ApplicationDbContext Context;
-//        protected IMapper Mapper;
+namespace Product.Test.Common
+{
+    public class TestQueryHandler : IDisposable
+    {
+        protected ApplicationDbContext Context;
+        protected IMapper Mapper;
 
-//        public TestQueryHandler()
-//        {
-//            Context = ProductDbContextFactory.Create();
+        public TestQueryHandler()
+        {
+            Context = ProductDbContextFactory.Create();
 
-//            var mapperConfiguration = new MapperConfiguration(config =>
-//            {
-//                config.AddProfile<MappingProfile>();
-//            });
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile<MappingProfile>();
+            });
 
-//            Mapper = mapperConfiguration.CreateMapper();
-//        }
+            Mapper = mapperConfiguration.CreateMapper();
+        }
 
-//        public void Dispose()
-//        {
-//            ProductDbContextFactory.Destroy(Context);
-//        }
-//    }
+        public void Dispose()
+        {
+            ProductDbContextFactory.Destroy(Context);
+        }
+    }
 
-//    [CollectionDefinition("QueryCollection")]
-//    public class QueryCollection : ICollectionFixture<TestQueryHandler> { }
-//}
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<TestQueryHandler> { }
+}
