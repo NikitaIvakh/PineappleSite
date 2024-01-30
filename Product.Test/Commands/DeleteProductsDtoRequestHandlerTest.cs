97f765c0d22fcf3b445a9c29e28074d78eac9187
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-30 20:28:10 +0300

    Update command

--- Product.Test/Commands/DeleteProductsDtoRequestHandlerTest.cs
+++ Product.Test/Commands/DeleteProductsDtoRequestHandlerTest.cs
@@ -1,58 +1,57 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using Product.Application.DTOs.Products;
-//using Product.Application.Features.Commands.Handlers;
-//using Product.Application.Features.Requests.Handlers;
-//using Product.Test.Common;
-//using Shouldly;
-//using Xunit;
+﻿using Product.Application.Features.Commands.Handlers;
+using Product.Application.Features.Requests.Handlers;
+using Product.Domain.DTOs;
+using Product.Test.Common;
+using Shouldly;
+using Xunit;
 
-//namespace Product.Test.Commands
-//{
-//    public class DeleteProductsDtoRequestHandlerTest : TestCommandHandler
-//    {
-//        [Fact]
-//        public async Task DeleteProductsDtoRequestHandlerTest_Success()
-//        {
-//            // Arrange
-//            var handler = new DeleteProductsDtoRequestHandler(Context, DeleteProductsValidator);
-//            var deleteProducts = new DeleteProductsDto
-//            {
-//                ProductIds = new List<int> { 1, 2 },
-//            };
+namespace Product.Test.Commands
+{
+    public class DeleteProductsDtoRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task DeleteProductsDtoRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new DeleteProductsDtoRequestHandler(Repository, DeleteProductsValidator, DeleteListLogger, Mapper);
+            var deleteProducts = new DeleteProductsDto
+            {
+                ProductIds = new List<int> { 1, 2 },
+            };
 
-//            // Act
-//            var result = await handler.Handle(new DeleteProductsDtoRequest
-//            {
-//                DeleteProducts = deleteProducts,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteProductsDtoRequest
+            {
+                DeleteProducts = deleteProducts,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.IsSuccess.ShouldBeTrue();
-//            result.Message.ShouldBe("Продукты успешно удалены");
-//            result.ValidationErrors.ShouldBeNull();
-//        }
+            // Assert
+            result.IsSuccess.ShouldBeTrue();
+            result.SuccessMessage.ShouldBe("Продукты успешно удалены");
+            result.ValidationErrors.ShouldBeNull();
+        }
 
-//        [Fact]
-//        public async Task DeleteProductsDtoRequestHandlerTest_FailOrWrongIds()
-//        {
-//            // Arrange
-//            var handler = new DeleteProductsDtoRequestHandler(Context, DeleteProductsValidator);
-//            var deleteProducts = new DeleteProductsDto
-//            {
-//                ProductIds = new List<int>(),
-//            };
+        [Fact]
+        public async Task DeleteProductsDtoRequestHandlerTest_FailOrWrongIds()
+        {
+            // Arrange
+            var handler = new DeleteProductsDtoRequestHandler(Repository, DeleteProductsValidator, DeleteListLogger, Mapper);
+            var deleteProducts = new DeleteProductsDto
+            {
+                ProductIds = new List<int>(),
+            };
 
 
-//            // Act
-//            var result = await handler.Handle(new DeleteProductsDtoRequest
-//            {
-//                DeleteProducts = deleteProducts,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteProductsDtoRequest
+            {
+                DeleteProducts = deleteProducts,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.IsSuccess.ShouldBeFalse();
-//            result.ValidationErrors.ShouldNotBeEmpty();
-//            result.ValidationErrors.ShouldNotBeNull();
-//        }
-//    }
-//}
+            // Assert
+            result.IsSuccess.ShouldBeFalse();
+            result.ValidationErrors.ShouldNotBeEmpty();
+            result.ValidationErrors.ShouldNotBeNull();
+        }
+    }
+}
