57786e78a25baf467a2887403dbb45cc67016a9f
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-30 20:38:52 +0300

    Update request

--- Product.Test/Queries/GetProductDetailsRequestHandlerTest.cs
+++ Product.Test/Queries/GetProductDetailsRequestHandlerTest.cs
@@ -1,45 +1,50 @@
-﻿//using Product.Application.Features.Commands.Queries;
-//using Product.Application.Features.Requests.Queries;
-//using Product.Test.Common;
-//using Shouldly;
-//using Xunit;
+﻿using FluentAssertions;
+using Product.Application.Features.Commands.Queries;
+using Product.Application.Features.Requests.Queries;
+using Product.Test.Common;
+using Shouldly;
+using Xunit;
 
-//namespace Product.Test.Queries
-//{
-//    public class GetProductDetailsRequestHandlerTest : TestQueryHandler
-//    {
-//        [Fact]
-//        public async Task GetProductDetailsRequestHandlerTest_Success()
-//        {
-//            // Arrange
-//            var handler = new GetProductDetailsRequestHandler(Context, Mapper);
-//            var producId = 4;
+namespace Product.Test.Queries
+{
+    public class GetProductDetailsRequestHandlerTest : TestQueryHandler
+    {
+        [Fact]
+        public async Task GetProductDetailsRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new GetProductDetailsRequestHandler(Repository, Logger, Mapper);
+            var producId = 4;
 
-//            // Act
-//            var result = await handler.Handle(new GetProductDetailsRequest
-//            {
-//                Id = producId,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new GetProductDetailsRequest
+            {
+                Id = producId,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.ShouldNotBeNull();
-//            result.Name.ShouldBe("Test 1");
-//            result.Description.ShouldBe("Test product 1");
-//            result.Price.ShouldBe(10);
-//        }
+            // Assert
+            result.ShouldNotBeNull();
+            result.Data.Name.ShouldBe("Test 1");
+            result.Data.Description.ShouldBe("Test product 1");
+            result.Data.Price.ShouldBe(10);
+        }
 
-//        [Fact]
-//        public async Task GetProductDetailsRequestHandlerTest_FailOrWrongId()
-//        {
-//            // Arrange
-//            var handler = new GetProductDetailsRequestHandler(Context, Mapper);
-//            var productId = 999;
+        [Fact]
+        public async Task GetProductDetailsRequestHandlerTest_FailOrWrongId()
+        {
+            // Arrange
+            var handler = new GetProductDetailsRequestHandler(Repository, Logger, Mapper);
+            var productId = 999;
 
-//            // Act && Assert
-//            await Assert.ThrowsAsync<Exception>(async () => await handler.Handle(new GetProductDetailsRequest
-//            {
-//                Id = productId,
-//            }, CancellationToken.None));
-//        }
-//    }
-//}
+            // Act && Assert
+            var result = await handler.Handle(new GetProductDetailsRequest
+            { 
+                Id = productId,
+            }, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.ErrorMessage.Should().Be("Продукт не найден");
+        }
+    }
+}
