397d50de815b47e4680a00f4cd2d101254d9a1c2
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-30 20:26:44 +0300

    Update command

--- Product.Test/Commands/DeleteProductDtoRequestHandlerTest.cs
+++ Product.Test/Commands/DeleteProductDtoRequestHandlerTest.cs
@@ -1,60 +1,58 @@
-﻿//using MediatR;
-//using Microsoft.AspNetCore.Http;
-//using Product.Application.DTOs.Products;
-//using Product.Application.Features.Commands.Handlers;
-//using Product.Application.Features.Requests.Handlers;
-//using Product.Application.Response;
-//using Product.Test.Common;
-//using Shouldly;
-//using Xunit;
+﻿using Product.Application.Features.Commands.Handlers;
+using Product.Application.Features.Requests.Handlers;
+using Product.Domain.DTOs;
+using Product.Domain.ResultProduct;
+using Product.Test.Common;
+using Shouldly;
+using Xunit;
 
-//namespace Product.Test.Commands
-//{
-//    public class DeleteProductDtoRequestHandlerTest : TestCommandHandler
-//    {
-//        [Fact]
-//        public async Task DeleteProductDtoRequestHandlerTest_Success()
-//        {
-//            // Arrange
-//            var handler = new DeleteProductDtoRequestHandler(Context, DeleteValidator);
-//            var deleteProductDto = new DeleteProductDto
-//            {
-//                Id = 3,
-//            };
+namespace Product.Test.Commands
+{
+    public class DeleteProductDtoRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task DeleteProductDtoRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new DeleteProductDtoRequestHandler(Repository, DeleteValidator, DeleteLogger, Mapper);
+            var deleteProductDto = new DeleteProductDto
+            {
+                Id = 3,
+            };
 
-//            // Act
-//            var result = await handler.Handle(new DeleteProductDtoRequest
-//            {
-//                DeleteProduct = deleteProductDto
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteProductDtoRequest
+            {
+                DeleteProduct = deleteProductDto
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.ShouldBeOfType<ProductAPIResponse>();
-//            result.IsSuccess.ShouldBeTrue();
-//            result.Message.ShouldBe("Продукт успешно удален");
-//            result.ValidationErrors.ShouldBeNull();
-//        }
+            // Assert
+            result.ShouldBeOfType<Result<ProductDto>>();
+            result.IsSuccess.ShouldBeTrue();
+            result.SuccessMessage.ShouldBe("Продукт успешно удален");
+            result.ValidationErrors.ShouldBeNull();
+        }
 
-//        [Fact]
-//        public async Task DeleteProductDtoRequestHandlerTest_FailOrWrong()
-//        {
-//            // Arrange
-//            var handler = new DeleteProductDtoRequestHandler(Context, DeleteValidator);
-//            var deleteProductDto = new DeleteProductDto
-//            {
-//                Id = 999,
-//            };
+        [Fact]
+        public async Task DeleteProductDtoRequestHandlerTest_FailOrWrong()
+        {
+            // Arrange
+            var handler = new DeleteProductDtoRequestHandler(Repository, DeleteValidator, DeleteLogger, Mapper);
+            var deleteProductDto = new DeleteProductDto
+            {
+                Id = 999,
+            };
 
-//            // Act
-//            var result = await handler.Handle(new DeleteProductDtoRequest
-//            {
-//                DeleteProduct = deleteProductDto
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteProductDtoRequest
+            {
+                DeleteProduct = deleteProductDto
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.IsSuccess.ShouldBeFalse();
-//            result.Message.ShouldBe($"Продукта c идентификатором: ({deleteProductDto.Id}) не найдено");
-//            result.ValidationErrors.ShouldBeNull();
-//        }
-//    }
-//}
+            // Assert
+            result.IsSuccess.ShouldBeFalse();
+            result.ErrorMessage.ShouldBe("Продукт не найден");
+            result.ValidationErrors.ShouldBeNull();
+        }
+    }
+}
