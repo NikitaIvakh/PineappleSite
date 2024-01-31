a9a037681c190c858c6bb5b2609e1f9e9bafd792
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-31 11:36:46 +0300

    Update tests for get coupon by id

--- Coupon.Test/Queries/GetCouponDetailsRequestHandlerTest.cs
+++ Coupon.Test/Queries/GetCouponDetailsRequestHandlerTest.cs
@@ -1,46 +1,58 @@
-﻿//using Coupon.Application.Features.Coupons.Handlers.Queries;
-//using Coupon.Application.Features.Coupons.Requests.Queries;
-//using Coupon.Test.Common;
-//using Shouldly;
-//using Xunit;
+﻿using Coupon.Application.Features.Coupons.Handlers.Queries;
+using Coupon.Application.Features.Coupons.Requests.Queries;
+using Coupon.Test.Common;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
 
-//namespace Coupon.Test.Queries
-//{
-//    public class GetCouponDetailsRequestHandlerTest : TestQueryHandler
-//    {
-//        [Fact]
-//        public async Task GetCouponDetailsRequestHandlerTest_Success()
-//        {
-//            // Arrange
-//            var handler = new GetCouponDetailsRequestHandler(Repository, Logger);
-//            int couponId = 1;
+namespace Coupon.Test.Queries
+{
+    public class GetCouponDetailsRequestHandlerTest : TestQueryHandler
+    {
+        [Fact]
+        public async Task GetCouponDetailsRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new GetCouponDetailsRequestHandler(Repository, GetLogger);
+            int couponId = 5;
 
-//            // Act
-//            var result = await handler.Handle(new GetCouponDetailsRequest
-//            {
-//                Id = couponId,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new GetCouponDetailsRequest
+            {
+                Id = couponId,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.ValidationErrors.ShouldBeNull();
-//        }
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.ErrorMessage.Should().BeNullOrEmpty();
+            result.ValidationErrors.ShouldBeNull();
 
-//        [Fact]
-//        public async Task GetCouponDetailsRequestHandlerTest_FailOrWrongId()
-//        {
-//            // Arrange
-//            var handler = new GetCouponDetailsRequestHandler(Repository, Logger);
-//            var couponId = 999;
+            var getCoupon = await Context.Coupons.AsNoTracking().FirstOrDefaultAsync(key => key.CouponId == result.Data.CouponId);
+            getCoupon.CouponId.Should().Be(5);
+            getCoupon.CouponCode.Should().Be("30OFF");
+            getCoupon.DiscountAmount.Should().Be(30);
+            getCoupon.MinAmount.Should().Be(40);
+        }
 
-//            // Act
-//            var result = await handler.Handle(new GetCouponDetailsRequest
-//            {
-//                Id = couponId,
-//            }, CancellationToken.None);
+        [Fact]
+        public async Task GetCouponDetailsRequestHandlerTest_FailOrWrongId()
+        {
+            // Arrange
+            var handler = new GetCouponDetailsRequestHandler(Repository, GetLogger);
+            var couponId = 999;
 
-//            // Assert
-//            result.ErrorMessage.ShouldBe("Внутренняя проблема сервера");
-            
-//        }
-//    }
-//}
+            // Act
+            var result = await handler.Handle(new GetCouponDetailsRequest
+            {
+                Id = couponId,
+            }, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.ErrorMessage.ShouldBe("Купон не найден");
+            result.SuccessMessage.Should().BeNullOrEmpty();
+
+        }
+    }
+}
