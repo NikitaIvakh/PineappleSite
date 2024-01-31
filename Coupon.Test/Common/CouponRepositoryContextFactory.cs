6672c177e261577b708ba74e527576298ed3512a
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-31 09:57:17 +0300

    Update CouponRepositoryContextFactory class

--- Coupon.Test/Common/CouponRepositoryContextFactory.cs
+++ Coupon.Test/Common/CouponRepositoryContextFactory.cs
@@ -1,5 +1,4 @@
 ﻿using Coupon.Domain.Entities;
-using Coupon.Domain.Interfaces.Repositories;
 using Coupon.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,35 +6,34 @@ namespace Coupon.Test.Common
 {
     public static class CouponRepositoryContextFactory
     {
-        public static ApplicationDbContext Create(IBaseRepository<CouponEntity> repository)
+        public static ApplicationDbContext Create()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
             var context = new ApplicationDbContext(options);
             context.Database.EnsureCreated();
-            repository.CreateAsync(new CouponEntity
+            context.AddRange(new CouponEntity
             {
                 CouponId = 3,
                 CouponCode = "10OFF",
                 DiscountAmount = 10,
                 MinAmount = 20,
-            }).GetAwaiter().GetResult();
-
-            repository.CreateAsync(new CouponEntity
+            },
+            new CouponEntity
             {
                 CouponId = 4,
                 CouponCode = "20OFF",
                 DiscountAmount = 20,
                 MinAmount = 30,
-            }).GetAwaiter().GetResult();
-
-            repository.CreateAsync(new CouponEntity
+            },
+            new CouponEntity
             {
                 CouponId = 5,
                 CouponCode = "30OFF",
                 DiscountAmount = 30,
                 MinAmount = 40,
-            }).GetAwaiter().GetResult();
+            });
 
+            context.SaveChanges();
             return context;
         }
 
