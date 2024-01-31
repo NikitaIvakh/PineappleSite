7fd47a3af258f25bb00e8e13ecd5356578ec824b
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-31 10:50:59 +0300

    Update tests for delete coupon

--- Coupon.Test/Commands/DeleteCouponRequestHandlerTest.cs
+++ Coupon.Test/Commands/DeleteCouponRequestHandlerTest.cs
@@ -1,53 +1,58 @@
-﻿//using Coupon.Application.Features.Coupons.Handlers.Commands;
-//using Coupon.Application.Features.Coupons.Requests.Commands;
-//using Coupon.Domain.DTOs;
-//using Coupon.Test.Common;
-//using FluentAssertions;
-//using Shouldly;
-//using Xunit;
+﻿using Coupon.Application.Features.Coupons.Handlers.Commands;
+using Coupon.Application.Features.Coupons.Requests.Commands;
+using Coupon.Domain.DTOs;
+using Coupon.Test.Common;
+using FluentAssertions;
+using Shouldly;
+using Xunit;
 
-//namespace Coupon.Test.Commands
-//{
-//    public class DeleteCouponRequestHandlerTest : TestCommandHandler
-//    {
-//        [Fact]
-//        public async Task DeleteCouponRequestHandlerTest_Success()
-//        {
-//            // Arrange
-//            var handler = new DeleteCouponRequestHandler(Repository, Logger, Mapper, DeleteValidator);
-//            var deleteCoupon = new DeleteCouponDto
-//            {
-//                Id = 1,
-//            };
+namespace Coupon.Test.Commands
+{
+    public class DeleteCouponRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task DeleteCouponRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new DeleteCouponRequestHandler(Repository, DeleteLogger, Mapper, DeleteValidator);
+            var deleteCoupon = new DeleteCouponDto
+            {
+                Id = 1,
+            };
 
-//            // Act
-//            var result = await handler.Handle(new DeleteCouponRequest
-//            {
-//                DeleteCoupon = deleteCoupon,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteCouponRequest
+            {
+                DeleteCoupon = deleteCoupon,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.ValidationErrors.ShouldBeNull();
-//        }
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.ErrorMessage.Should().BeNullOrEmpty();
+            result.SuccessMessage.Should().Be("Купон успешно удален");
+            result.ValidationErrors.ShouldBeNull();
+        }
 
-//        [Fact]
-//        public async Task DeleteCouponRequestHandlerTest_FailOrWrongId()
-//        {
-//            // Arrange
-//            var handler = new DeleteCouponRequestHandler(Repository, Logger, Mapper, DeleteValidator);
-//            var deleteCoupon = new DeleteCouponDto
-//            {
-//                Id = 88,
-//            };
+        [Fact]
+        public async Task DeleteCouponRequestHandlerTest_FailOrWrongId()
+        {
+            // Arrange
+            var handler = new DeleteCouponRequestHandler(Repository, DeleteLogger, Mapper, DeleteValidator);
+            var deleteCoupon = new DeleteCouponDto
+            {
+                Id = 88,
+            };
 
-//            // Act
-//            var result = await handler.Handle(new DeleteCouponRequest
-//            {
-//                DeleteCoupon = deleteCoupon,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteCouponRequest
+            {
+                DeleteCoupon = deleteCoupon,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.ValidationErrors.Should().BeNull();
-//        }
-//    }
-//}
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.ErrorMessage.Should().Be("Купон не может быть удален");
+            result.SuccessMessage.Should().BeNullOrEmpty();
+        }
+    }
+}
