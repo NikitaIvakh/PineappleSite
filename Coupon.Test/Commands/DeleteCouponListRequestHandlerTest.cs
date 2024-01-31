a82ff69ee246fec82241bbf1a502cc3f5f8d8147
Nikita Ivakh <maffy145rus@mail.ru>
2024-01-31 10:46:57 +0300

    Update tests for delete coupon list

--- Coupon.Test/Commands/DeleteCouponListRequestHandlerTest.cs
+++ Coupon.Test/Commands/DeleteCouponListRequestHandlerTest.cs
@@ -1,52 +1,57 @@
-﻿//using Coupon.Application.Features.Coupons.Handlers.Commands;
-//using Coupon.Application.Features.Coupons.Requests.Commands;
-//using Coupon.Domain.DTOs;
-//using Coupon.Domain.ResultCoupon;
-//using Coupon.Test.Common;
-//using FluentAssertions;
-//using Shouldly;
-//using Xunit;
+﻿using Coupon.Application.Features.Coupons.Handlers.Commands;
+using Coupon.Application.Features.Coupons.Requests.Commands;
+using Coupon.Domain.DTOs;
+using Coupon.Test.Common;
+using FluentAssertions;
+using Shouldly;
+using Xunit;
 
-//namespace Coupon.Test.Commands
-//{
-//    public class DeleteCouponListRequestHandlerTest : TestCommandHandler
-//    {
-//        [Fact]
-//        public async Task DeleteCouponListRequestHandlerTest_Success()
-//        {
-//            // Arrange
-//            var handler = new DeleteCouponListRequestHandler(Repository, Logger, Mapper, DeleteListValidator);
-//            var deleteCouponList = new DeleteCouponListDto
-//            {
-//                CouponIds = [1, 2, 3],
-//            };
+namespace Coupon.Test.Commands
+{
+    public class DeleteCouponListRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task DeleteCouponListRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new DeleteCouponListRequestHandler(Repository, DeleteListLogger, Mapper, DeleteListValidator);
+            var deleteCouponList = new DeleteCouponListDto
+            {
+                CouponIds = [1, 2, 3],
+            };
 
-//            // Act
-//            var result = await handler.Handle(new DeleteCouponListRequest
-//            {
-//                DeleteCoupon = deleteCouponList,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteCouponListRequest
+            {
+                DeleteCoupon = deleteCouponList,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.ValidationErrors.Should().BeNull();
-//        }
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.ErrorMessage.Should().BeNullOrEmpty();
+            result.ValidationErrors.Should().BeNull();
+            result.SuccessMessage.Should().Be("Купоны успешно удалены");
+        }
 
-//        [Fact]
-//        public async Task DeleteCouponListRequestHandlerTest_EmptyList()
-//        {
-//            var handler = new DeleteCouponListRequestHandler(Repository, Logger, Mapper, DeleteListValidator);
-//            var deleteCouponList = new DeleteCouponListDto
-//            {
-//                CouponIds = [],
-//            };
+        [Fact]
+        public async Task DeleteCouponListRequestHandlerTest_EmptyList()
+        {
+            var handler = new DeleteCouponListRequestHandler(Repository, DeleteListLogger, Mapper, DeleteListValidator);
+            var deleteCouponList = new DeleteCouponListDto
+            {
+                CouponIds = [],
+            };
 
-//            var result = await handler.Handle(new DeleteCouponListRequest
-//            {
-//                DeleteCoupon = deleteCouponList,
-//            }, CancellationToken.None);
+            var result = await handler.Handle(new DeleteCouponListRequest
+            {
+                DeleteCoupon = deleteCouponList,
+            }, CancellationToken.None);
 
-//            // Assert
-//            result.IsSuccess.ShouldBeFalse();
-//        }
-//    }
-//}
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.ErrorMessage.Should().Be("Таких купонов не существует");
+            result.ValidationErrors.Should().NotBeNullOrEmpty();
+            result.SuccessMessage.Should().BeNullOrEmpty();
+        }
+    }
+}
