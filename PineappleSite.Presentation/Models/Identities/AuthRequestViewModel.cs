9f92ffe8fdb96b917f36a9eb49e8aff392cf7f44
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-02 12:59:57 +0300

    Add attributes

--- PineappleSite.Presentation/Models/Identities/AuthRequestViewModel.cs
+++ PineappleSite.Presentation/Models/Identities/AuthRequestViewModel.cs
@@ -1,9 +1,20 @@
-﻿namespace PineappleSite.Presentation.Models.Identities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PineappleSite.Presentation.Models.Identities
 {
     public class AuthRequestViewModel
     {
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Ведите валидный адрес электронной почты")]
+        [Required(ErrorMessage = "Адрес электронной почты не может быть пустым")]
+        [MinLength(2, ErrorMessage = "Адрес электронной почты должен быть более 2 символов")]
+        [MaxLength(50, ErrorMessage = "Адрес электронной почты не может превышать 50 символов")]
         public string Email { get; set; }
 
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
+        [MinLength(5, ErrorMessage = "Пароль должен быть более 5 символов")]
+        [MaxLength(50, ErrorMessage = "Пароль не может превышать 50 символов")]
         public string Password { get; set; }
 
         public string? ReturnUrl { get; set; }
