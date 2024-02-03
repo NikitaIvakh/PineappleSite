3f9433b48a1c4e4eac79d790263cc17f60299ba9
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-03 18:25:28 +0300

    Add validation attributes

--- PineappleSite.Presentation/Models/Products/CreateProductViewModel.cs
+++ PineappleSite.Presentation/Models/Products/CreateProductViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using PineappleSite.Presentation.Models.Products.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace PineappleSite.Presentation.Models.Products
 {
     public class CreateProductViewModel
     {
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [MaxLength(30, ErrorMessage = "Строка не должна превышать 30 символов")]
+        [MinLength(3, ErrorMessage = "Строка должна быть более 3 символов")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [MaxLength(500, ErrorMessage = "Строка не должна превышать 500 символов")]
+        [MinLength(10, ErrorMessage = "Строка должна быть более 10 символов")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
         public ProductCategory ProductCategory { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [Range(5, 1000, ErrorMessage = "Стоимость товара доложна быть более 5, но не более 1000")]
         public double Price { get; set; }
 
         public IFormFile? Avatar { get; set; }
