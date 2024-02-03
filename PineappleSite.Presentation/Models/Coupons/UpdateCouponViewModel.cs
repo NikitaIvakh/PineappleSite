c2a8c48bfba5fde6fd2547d34a2ab098b4676f42
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-03 14:48:41 +0300

    Add validation attributes

--- PineappleSite.Presentation/Models/Coupons/UpdateCouponViewModel.cs
+++ PineappleSite.Presentation/Models/Coupons/UpdateCouponViewModel.cs
@@ -1,13 +1,19 @@
-﻿namespace PineappleSite.Presentation.Models.Coupons
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PineappleSite.Presentation.Models.Coupons
 {
     public class UpdateCouponViewModel
     {
         public int CouponId { get; set; }
 
+        [MaxLength(20, ErrorMessage = "Строка не должна превышать 20 символов")]
+        [MinLength(3, ErrorMessage = "Строка должна превышать 3 символа")]
         public string CouponCode { get; set; }
 
+        [Range(2, 101, ErrorMessage = "Сумма скидки должна превышать 2 единицы, но не превышать 101 единицу")]
         public double DiscountAmount { get; set; }
 
+        [Range(2, 101, ErrorMessage = "Стоимость товара должна превышать 2 единицы, но не превышать 101 единицу")]
         public double MinAmount { get; set; }
     }
 }
