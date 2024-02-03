831f17f38eaa82404185702fbd83c84b18a3ce90
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-03 18:27:28 +0300

    Add validation attributes

--- PineappleSite.Presentation/Models/Products/UpdateProductViewModel.cs
+++ PineappleSite.Presentation/Models/Products/UpdateProductViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using PineappleSite.Presentation.Models.Products.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace PineappleSite.Presentation.Models.Products
 {
@@ -6,12 +7,17 @@ namespace PineappleSite.Presentation.Models.Products
     {
         public int Id { get; set; }
 
+        [MaxLength(30, ErrorMessage = "Строка не должна превышать 30 символов")]
+        [MinLength(3, ErrorMessage = "Строка должна быть более 3 символов")]
         public string Name { get; set; }
 
+        [MaxLength(500, ErrorMessage = "Строка не должна превышать 500 символов")]
+        [MinLength(10, ErrorMessage = "Строка должна быть более 10 символов")]
         public string Description { get; set; }
 
         public ProductCategory ProductCategory { get; set; }
 
+        [Range(5, 1000, ErrorMessage = "Стоимость товара доложна быть более 5, но не более 1000")]
         public double Price { get; set; }
 
         public IFormFile? Avatar { get; set; }
