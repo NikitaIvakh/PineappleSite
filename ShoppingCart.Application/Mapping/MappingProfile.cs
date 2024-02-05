94d7e4ecb4b0da12236f81075ac2af4ad38020fd
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-05 12:36:47 +0300

    Ad Mapping

---
+++ ShoppingCart.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Favourites.Domain.DTOs;
+using ShoppingCart.Domain.Entities;
+
+namespace ShoppingCart.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region ShoppingCart Mapping
+            CreateMap<CartHeader, CartHeader>();
+            CreateMap<CartDetails, CartDetailsDto>();
+            #endregion
+        }
+    }
+}
