7a7d7522c33914fd4a78d6364d2079e680871236
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-05 15:24:04 +0300

    Create ShoppingCartUpsert command

---
+++ ShoppingCart.Application/Features/Handlers/Commands/ShoppingCartUpsertRequestHandler.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Favourites.Domain.DTOs;
+using MediatR;
+using ShoppingCart.Application.Features.Requests.Commands;
+using ShoppingCart.Application.Resources;
+using ShoppingCart.Domain.Entities;
+using ShoppingCart.Domain.Enum;
+using ShoppingCart.Domain.Interfaces.Repository;
+using ShoppingCart.Domain.Results;
+
+namespace ShoppingCart.Application.Features.Handlers.Commands
+{
+    public class ShoppingCartUpsertRequestHandler(IBaseRepository<CartHeader> cartHeaderRepository, IBaseRepository<CartDetails> cartDetailsRepository, IMapper mapper) : IRequestHandler<ShoppingCartUpsertRequest, Result<CartDto>>
+    {
+        private readonly IBaseRepository<CartHeader> _cartHeaderRepository = cartHeaderRepository;
+        private readonly IBaseRepository<CartDetails> _cartDetailsRepository = cartDetailsRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Result<CartDto>> Handle(ShoppingCartUpsertRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var cartHeaderFromDb = _cartHeaderRepository.GetAll().FirstOrDefault(key => key.UserId == request.CartDto.CartHeader.UserId);
+
+                if (cartHeaderFromDb is null)
+                {
+                    CartHeader? cartHeader = _mapper.Map<CartHeader>(request.CartDto.CartHeader);
+                    await _cartHeaderRepository.CreateAsync(cartHeader);
+
+                    request.CartDto.CartDetails.First().CartHeaderId = cartHeader.CartHeaderId;
+                    await _cartDetailsRepository.CreateAsync(_mapper.Map<CartDetails>(request.CartDto.CartDetails.First()));
+                }
+
+                else
+                {
+                    var cartDetailsFromDb = _cartDetailsRepository
+                        .GetAll()
+                        .FirstOrDefault(key => key.ProductId == request.CartDto.CartDetails.First().ProductId && key.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+
+                    if (cartHeaderFromDb is null)
+                    {
+                        request.CartDto.CartDetails.First().CartHeaderId = cartHeaderFromDb.CartHeaderId;
+                        await _cartDetailsRepository.CreateAsync(_mapper.Map<CartDetails>(request.CartDto.CartDetails.First()));
+                    }
+
+                    else
+                    {
+                        request.CartDto.CartDetails.First().CartDetailsId = cartDetailsFromDb.CartDetailsId;
+                        request.CartDto.CartDetails.First().Count += cartDetailsFromDb.Count;
+                        request.CartDto.CartDetails.First().CartHeaderId = cartHeaderFromDb.CartHeaderId;
+
+                        await _cartDetailsRepository.UpdateAsync(_mapper.Map<CartDetails>(request.CartDto.CartDetails.First()));
+                    }
+                }
+
+                return new Result<CartDto>
+                {
+                    Data = request.CartDto,
+                    SuccessMessage = "Товар успешно добавлен в корзину",
+                };
+            }
+
+            catch (Exception exception)
+            {
+                return new Result<CartDto>
+                {
+                    ErrorMessage = ErrorMessages.InternalServerError,
+                    ErrorCode = (int)ErrorCodes.InternalServerError,
+                    ValidationErrors = new List<string> { exception.Message }
+                };
+            }
+        }
+    }
+}
---
+++ ShoppingCart.Application/Features/Requests/Commands/ShoppingCartUpsertRequest.cs
@@ -0,0 +1,11 @@
+﻿using Favourites.Domain.DTOs;
+using MediatR;
+using ShoppingCart.Domain.Results;
+
+namespace ShoppingCart.Application.Features.Requests.Commands
+{
+    public class ShoppingCartUpsertRequest : IRequest<Result<CartDto>>
+    {
+        public CartDto CartDto { get; set; }
+    }
+}
