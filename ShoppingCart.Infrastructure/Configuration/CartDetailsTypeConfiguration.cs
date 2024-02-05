271aceb04acf6f1d190b27e7ce9687f5231b55da
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-05 12:20:15 +0300

    Add configurations

---
+++ ShoppingCart.Infrastructure/Configuration/CartDetailsTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoppingCart.Domain.Entities;
+
+namespace ShoppingCart.Infrastructure.Configuration
+{
+    public class CartDetailsTypeConfiguration : IEntityTypeConfiguration<CartDetails>
+    {
+        public void Configure(EntityTypeBuilder<CartDetails> builder)
+        {
+            builder.HasKey(key => key.CartDetailsId);
+            builder.HasOne(key => key.CartHeader).WithMany().HasForeignKey(key => key.CartHeaderId);
+
+            builder.Ignore(key => key.Product);
+        }
+    }
+}
