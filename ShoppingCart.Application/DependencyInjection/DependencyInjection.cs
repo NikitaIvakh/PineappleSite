2374ceebc35e7e2dde20bd18632588a61057e9a9
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-05 12:33:24 +0300

    Add DependencyInjection class

---
+++ ShoppingCart.Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ShoppingCart.Application.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+
+        }
+
+        private static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(config => config.RegisterServicesFromAssemblies([Assembly.GetExecutingAssembly()]));
+        }
+    }
+}
