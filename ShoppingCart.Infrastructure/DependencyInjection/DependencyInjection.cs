f3bddb2614514966eee80e89575a8a41d11c3b27
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-05 12:15:16 +0300

    Create DependencyInjection class

---
+++ ShoppingCart.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingCart.Domain.Entities;
+using ShoppingCart.Domain.Interfaces.Repository;
+using ShoppingCart.Infrastructure.Repository.Implement;
+
+namespace ShoppingCart.Infrastructure.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.RegisterServices();
+            services.RegisterDataBase(configuration);
+        }
+
+        private static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository<CartHeader>, BaseRepository<CartHeader>>();
+            services.AddScoped<IBaseRepository<CartDetails>, BaseRepository<CartDetails>>();
+        }
+
+        private static void RegisterDataBase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(config =>
+            {
+                config.UseNpgsql(configuration.GetConnectionString("ShoppingCartDbConnection");
+            });
+        }
+
+        private static void ApplyMigration(this IServiceCollection services)
+        {
+            var scope = services.BuildServiceProvider();
+            using var serviceProvider = scope.CreateScope();
+            var db = serviceProvider.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.Migrate();
+        }
+    }
+}
