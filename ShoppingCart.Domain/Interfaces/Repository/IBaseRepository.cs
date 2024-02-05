a8d34788ea5e68d5976a880a8bdc5dbb5d4daf6f
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-05 11:57:17 +0300

    Create interface

---
+++ ShoppingCart.Domain/Interfaces/Repository/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingCart.Domain.Interfaces.Repository
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> CreateAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+    }
+}
