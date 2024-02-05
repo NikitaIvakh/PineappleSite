dd8e5718d98ee0ce34b5bf6567d97728ee2f975d
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-05 17:02:34 +0300

    Create ApplyCoupon command

---
+++ ShoppingCart.Application/Features/Handlers/Commands/ApplyCouponRequestHandler.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Favourites.Domain.DTOs;
+using MediatR;
+using ShoppingCart.Application.Features.Requests.Commands;
+using ShoppingCart.Application.Resources;
+using ShoppingCart.Domain.Entities;
+using ShoppingCart.Domain.Enum;
+using ShoppingCart.Domain.Interfaces.Repository;
+using ShoppingCart.Domain.Results;
+
+namespace ShoppingCart.Application.Features.Handlers.Commands
+{
+    public class ApplyCouponRequestHandler(IBaseRepository<CartHeader> cartHeaderRepository, IMapper mapper) : IRequestHandler<ApplyCouponRequest, Result<CartHeaderDto>>
+    {
+        private readonly IBaseRepository<CartHeader> _cartHeaderRepository = cartHeaderRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Result<CartHeaderDto>> Handle(ApplyCouponRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                CartHeader? cartHeader = _cartHeaderRepository.GetAll().FirstOrDefault(key => key.CouponCode == request.CartDto.CartHeader.CouponCode);
+
+                if (cartHeader is null)
+                {
+                    return new Result<CartHeaderDto>
+                    {
+                        ErrorMessage = ErrorMessages.InternalServerError,
+                        ErrorCode = (int)ErrorCodes.InternalServerError,
+                    };
+                }
+
+                else
+                {
+                    cartHeader.CouponCode = request.CartDto.CartHeader.CouponCode;
+                    await _cartHeaderRepository.UpdateAsync(cartHeader);
+
+                    return new Result<CartHeaderDto>
+                    {
+                        Data = _mapper.Map<CartHeaderDto>(cartHeader),
+                        SuccessMessage = "Купон успешно применен",
+                    };
+                }
+            }
+
+            catch (Exception exception)
+            {
+                return new Result<CartHeaderDto>
+                {
+                    ErrorMessage = ErrorMessages.InternalServerError,
+                    ErrorCode = (int)ErrorCodes.InternalServerError,
+                    ValidationErrors = new List<string> { exception.Message }
+                };
+            }
+        }
+    }
+}
