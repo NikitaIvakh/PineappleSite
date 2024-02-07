0cf86e4c8239b5f000ffaa149133fe248157dd8a
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 12:11:53 +0300

    Create configuration

---
+++ Favourite.Infrastructure/Configuration/CartDetailsTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Favourite.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Favourite.Infrastructure.Configuration
+{
+    public class CartDetailsTypeConfiguration : IEntityTypeConfiguration<FavouriteDetails>
+    {
+        public void Configure(EntityTypeBuilder<FavouriteDetails> builder)
+        {
+            builder.HasKey(key => key.FavouriteDetailsId);
+            builder.HasOne(key => key.FavouriteHeader).WithMany().HasForeignKey(key => key.FavouriteHeaderId);
+
+            builder.Ignore(key => key.Product);
+        }
+    }
+}
---
+++ Favourite.Infrastructure/Configuration/CartHeaderTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Favourite.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Favourite.Infrastructure.Configuration
+{
+    public class CartHeaderTypeConfiguration : IEntityTypeConfiguration<FavouriteHeader>
+    {
+        public void Configure(EntityTypeBuilder<FavouriteHeader> builder)
+        {
+            builder.HasKey(key => key.FavouriteHeaderId);
+            builder.Property(key => key.UserId).IsRequired(false);
+        }
+    }
+}
