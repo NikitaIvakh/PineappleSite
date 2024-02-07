c34650ab0eb9a2e40ab6b28df4f4b0f8255c535e
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 17:52:59 +0300

    Create FavouriteResult and FavouriteCollectionResult classes

---
+++ PineappleSite.Presentation/Services/Favorites/FavouriteResult.cs
@@ -0,0 +1,44 @@
+﻿namespace PineappleSite.Presentation.Services.Favorites
+{
+    public class FavouriteResult
+    {
+        public bool IsSuccess => ErrorMessage == null;
+
+        public string? ErrorMessage { get; set; }
+
+        public string? SuccessMessage { get; set; }
+
+        public int? ErrorCode { get; set; }
+
+        public List<string>? ValidationErrors { get; set; }
+    }
+
+    public class FavouriteResult<TEntity> : FavouriteResult
+    {
+        public FavouriteResult(string? errorMessage, int? errorCode, TEntity? data)
+        {
+            ErrorMessage = errorMessage;
+            ErrorCode = errorCode;
+            Data = data;
+        }
+
+        public FavouriteResult(string? errorMessage, int? errorCode, List<string>? validationErrors)
+        {
+            ErrorMessage = errorMessage;
+            ErrorCode = errorCode;
+            ValidationErrors = validationErrors;
+        }
+
+        public FavouriteResult(TEntity? data)
+        {
+            Data = data;
+        }
+
+        public FavouriteResult()
+        {
+
+        }
+
+        public TEntity? Data { get; set; }
+    }
+}
