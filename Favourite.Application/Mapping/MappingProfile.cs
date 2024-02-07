85c1dc10006245fdb139d62a64890e6c81d5a869
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 12:39:17 +0300

    Create mapping

---
+++ Favourite.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Favourite.Domain.DTOs;
+using Favourite.Domain.Entities;
+
+namespace Favourite.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<FavouriteHeader, FavouriteHeaderDto>().ReverseMap();
+            CreateMap<FavouriteDetails, FavouriteDetailsDto>().ReverseMap();
+        }
+    }
+}
