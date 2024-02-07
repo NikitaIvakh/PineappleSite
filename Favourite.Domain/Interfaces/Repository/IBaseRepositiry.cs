953586c30417e7667bce95a7b1f0d116e374e537
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 12:00:23 +0300

    Create interface

---
+++ Favourite.Domain/Interfaces/Repository/IBaseRepositiry.cs
@@ -0,0 +1,13 @@
+﻿namespace Favourite.Domain.Interfaces.Repository
+{
+    public interface IBaseRepositiry<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> CreateAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+    }
+}
