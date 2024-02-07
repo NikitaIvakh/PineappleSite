b2a54705873008db5741038647a345b0db54eea7
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 12:47:44 +0300

    Create service IProductService

---
+++ Favourite.Domain/Interfaces/Services/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Favourite.Domain.DTOs;
+using Favourite.Domain.Results;
+
+namespace Favourite.Domain.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<CollectionResult<ProductDto>> GetProductListAsync();
+    }
+}
---
+++ Favourite.Infrastructure/Repository/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using Favourite.Domain.DTOs;
+using Favourite.Domain.Interfaces.Services;
+using Favourite.Domain.Results;
+using System.Text.Json;
+
+namespace Favourite.Infrastructure.Repository.Services
+{
+    public class ProductService(IHttpClientFactory httpClientFactory) : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+        public async Task<CollectionResult<ProductDto>> GetProductListAsync()
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient("Product");
+            HttpResponseMessage response = await httpClient.GetAsync($"/api/Product");
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var stream = await response.Content.ReadAsStreamAsync();
+                JsonSerializerOptions jsonSerializerOptions = new()
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+
+                var options = jsonSerializerOptions;
+                return await JsonSerializer.DeserializeAsync<CollectionResult<ProductDto>>(stream, options);
+            }
+
+            return new CollectionResult<ProductDto>();
+        }
+    }
+}
