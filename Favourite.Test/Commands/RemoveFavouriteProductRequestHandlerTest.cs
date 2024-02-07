30ecfe1114dc9a636c9a6ea81dea5225edc1c9f8
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 20:28:53 +0300

    Create test for remove product from favourite

---
+++ Favourite.Test/Commands/RemoveFavouriteProductRequestHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿using Favourite.Application.Features.Handlers.Commands;
+using Favourite.Application.Features.Requests.Commands;
+using Favourite.Test.Common;
+using FluentAssertions;
+using Xunit;
+
+namespace Favourite.Test.Commands
+{
+    public class RemoveFavouriteProductRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task RemoveFavoriteRequestHandlerTest_FailOrWrongId()
+        {
+            // Arrange
+            var handler = new RemoveFavouriteProductRequestHandler(FavouriteHeader, FavouriteDetails, Mapper);
+            var productId = 13;
+
+            // Act
+            var result = await handler.Handle(new RemoveFavouriteProductRequest
+            {
+                ProductId = productId,
+            }, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.ErrorMessage.Should().Be("Внутренняя ошибка сервера");
+            result.ErrorCode.Should().Be(500);
+        }
+    }
+}
