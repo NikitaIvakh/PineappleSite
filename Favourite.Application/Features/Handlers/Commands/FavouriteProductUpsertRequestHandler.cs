3218fac28641c6c0b9de9daa10c10a0f57f0fa39
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 16:17:33 +0300

    Create FavouriteProductUpsert command

---
+++ Favourite.Application/Features/Handlers/Commands/FavouriteProductUpsertRequestHandler.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Favourite.Application.Features.Requests.Commands;
+using Favourite.Application.Resources;
+using Favourite.Domain.DTOs;
+using Favourite.Domain.Entities;
+using Favourite.Domain.Enum;
+using Favourite.Domain.Interfaces.Repository;
+using Favourite.Domain.Results;
+using MediatR;
+
+namespace Favourite.Application.Features.Handlers.Commands
+{
+    public class FavouriteProductUpsertRequestHandler(IBaseRepositiry<FavouriteHeader> headerRepository, IBaseRepositiry<FavouriteDetails> detailsRepository, IMapper mapper) : IRequestHandler<FavouriteProductUpsertRequest, Result<FavouriteHeaderDto>>
+    {
+        private readonly IBaseRepositiry<FavouriteHeader> _favouriteHeaderRepository = headerRepository;
+        private readonly IBaseRepositiry<FavouriteDetails> _favouriteDetailsRepository = detailsRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Result<FavouriteHeaderDto>> Handle(FavouriteProductUpsertRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                FavouriteHeader? favouriteHeaderFromDb = _favouriteHeaderRepository.GetAll().FirstOrDefault(key => key.UserId == request.FavouriteDto.FavouriteHeader.UserId);
+
+                if (favouriteHeaderFromDb is null)
+                {
+                    FavouriteHeader? favouriteHeader = _mapper.Map<FavouriteHeader>(favouriteHeaderFromDb);
+                    await _favouriteHeaderRepository.CreateAsync(favouriteHeader);
+
+                    request.FavouriteDto.FavouriteDetails.First().FavouriteHeaderId = favouriteHeader.FavouriteHeaderId;
+                    await _favouriteDetailsRepository.CreateAsync(_mapper.Map<FavouriteDetails>(request.FavouriteDto.FavouriteDetails.First()));
+
+                    return new Result<FavouriteHeaderDto>
+                    {
+                        SuccessMessage = "Ваши избранные товары",
+                        Data = _mapper.Map<FavouriteHeaderDto>(favouriteHeader),
+                    };
+                }
+
+                else
+                {
+                    FavouriteDetails? favouriteDetails = _favouriteDetailsRepository
+                        .GetAll()
+                        .FirstOrDefault(key => key.ProductId == request.FavouriteDto.FavouriteDetails.First().ProductId && key.FavouriteHeaderId == favouriteHeaderFromDb.FavouriteHeaderId);
+
+                    if (favouriteDetails is null)
+                    {
+                        request.FavouriteDto.FavouriteDetails.First().FavouriteHeaderId = favouriteHeaderFromDb.FavouriteHeaderId;
+                        await _favouriteDetailsRepository.CreateAsync(_mapper.Map<FavouriteDetails>(request.FavouriteDto.FavouriteDetails.First()));
+                    }
+
+                    else
+                    {
+                        request.FavouriteDto.FavouriteDetails.First().FavouriteHeaderId = favouriteDetails.FavouriteHeaderId;
+                        request.FavouriteDto.FavouriteDetails.First().FavouriteDetailsId = favouriteDetails.FavouriteDetailsId;
+                    }
+
+                    return new Result<FavouriteHeaderDto>
+                    {
+                        SuccessMessage = "Ваши избранные товары",
+                        Data = _mapper.Map<FavouriteHeaderDto>(favouriteHeaderFromDb),
+                    };
+                }
+            }
+
+            catch (Exception exception)
+            {
+                return new Result<FavouriteHeaderDto>
+                {
+                    ErrorMessage = ErrorMessages.InternalServerError,
+                    ErrorCode = (int)ErrorCodes.InternalServerError,
+                };
+            }
+        }
+    }
+}
---
+++ Favourite.Application/Features/Requests/Commands/FavouriteProductUpsertRequest.cs
@@ -0,0 +1,11 @@
+﻿using Favourite.Domain.DTOs;
+using Favourite.Domain.Results;
+using MediatR;
+
+namespace Favourite.Application.Features.Requests.Commands
+{
+    public class FavouriteProductUpsertRequest : IRequest<Result<FavouriteHeaderDto>>
+    {
+        public FavouriteDto FavouriteDto { get; set; }
+    }
+}
