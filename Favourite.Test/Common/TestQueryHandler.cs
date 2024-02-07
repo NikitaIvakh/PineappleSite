9103dcf554658e9851cce37e9d7487a8f8608ad8
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 19:42:58 +0300

    Create TestQueryHandler for test queries

---
+++ Favourite.Test/Common/TestQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Favourite.Domain.Entities;
+using Favourite.Domain.Interfaces.Repository;
+using Favourite.Domain.Interfaces.Services;
+using Favourite.Infrastructure.Repository.Implement;
+using Favourite.Infrastructure;
+using Moq;
+using Xunit;
+
+namespace Favourite.Test.Common
+{
+    public class TestQueryHandler : IDisposable
+    {
+        protected ApplicationDbContext Context;
+        protected IMapper Mapper;
+
+        protected IBaseRepositiry<FavouriteHeader> FavouriteHeader;
+        protected IBaseRepositiry<FavouriteDetails> FavouriteDetails;
+        protected IProductService ProductService;
+
+        public TestQueryHandler()
+        {
+            var productMock = new Mock<IProductService>();
+
+            Context = FavouriteProductsDbContextFactory.Create();
+            FavouriteHeader = new BaseRepository<FavouriteHeader>(Context);
+            FavouriteDetails = new BaseRepository<FavouriteDetails>(Context);
+            ProductService = productMock.Object;
+        }
+
+        public void Dispose()
+        {
+            FavouriteProductsDbContextFactory.Destroy(Context);
+        }
+    }
+
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<TestQueryHandler> { }
+}
