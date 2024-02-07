0923881ac93d4e87c66d5963887a6d061637a9eb
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 10:41:03 +0300

    Create GetShoppingCartRequestHandlerTest test

--- ShoppingCart.Test/Common/TestQueryHandler.cs
+++ ShoppingCart.Test/Common/TestQueryHandler.cs
@@ -5,11 +5,15 @@ using ShoppingCart.Domain.Entities;
 using ShoppingCart.Domain.Interfaces.Repository;
 using ShoppingCart.Domain.Interfaces.Service;
 using ShoppingCart.Infrastructure;
+using ShoppingCart.Domain.DTOs;
+using ShoppingCart.Domain.Results;
 using ShoppingCart.Infrastructure.Repository.Implement;
+using ShoppingCart.Infrastructure.Repository.Services;
 using Xunit;
 
 namespace ShoppingCart.Test.Common
 {
+    [CollectionDefinition("QueryCollection")]
     public class TestQueryHandler
     {
         protected ApplicationDbContext Context;
@@ -22,14 +26,31 @@ namespace ShoppingCart.Test.Common
 
         public TestQueryHandler()
         {
-            var productObject = new Mock<IProductService>();
-            var couponObject = new Mock<ICouponService>();
+            var productMock = new Mock<IProductService>();
+
+            productMock.Setup(mock => mock.GetProductListAsync())
+                .ReturnsAsync(new CollectionResult<ProductDto>
+                {
+                    Data = new List<ProductDto>
+                    {
+                        new() { Id = 1, Name = "Product 1", Price = 10.0 },
+                        new() { Id = 2, Name = "Product 2", Price = 20.0 },
+                    }
+                });
+
+            var couponMock = new Mock<ICouponService>();
+
+            couponMock.Setup(mock => mock.GetCouponAsync("5OFF"))
+                .ReturnsAsync(new Result<CouponDto>
+                {
+                    Data = new CouponDto { CouponId = 1, CouponCode = "5OFF", DiscountAmount = 20, MinAmount = 40 }
+                });
 
             Context = ShoppingCartDbContextFactory.Create();
             CartHeader = new BaseRepository<CartHeader>(Context);
             CartDetails = new BaseRepository<CartDetails>(Context);
-            ProductService = productObject.Object;
-            CouponService = couponObject.Object;
+            ProductService = productMock.Object;
+            CouponService = couponMock.Object;
 
             var mapperComfiguration = new MapperConfiguration(config =>
             {
---
+++ ShoppingCart.Test/Queries/GetShoppingCartRequestHandlerTest.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using ShoppingCart.Application.Features.Handlers.Queries;
+using ShoppingCart.Application.Features.Requests.Queries;
+using ShoppingCart.Test.Common;
+using Xunit;
+
+namespace ShoppingCart.Test.Queries
+{
+    [CollectionDefinition("QueryCollection")]
+    public class GetShoppingCartRequestHandlerTest : TestQueryHandler
+    {
+        [Fact]
+        public async Task GetShoppingCartRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new GetShoppingCartRequestHandler(CartHeader, CartDetails, ProductService, CouponService, Mapper);
+            var userId = "TestuserId23";
+
+            // Act
+            var result = await handler.Handle(new GetShoppingCartRequest
+            {
+                UserId = userId,
+            }, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.ErrorMessage.Should().BeNullOrEmpty();
+            result?.Data?.CartHeader.CouponCode.Should().Be("5OFF");
+        }
+
+        [Fact]
+        public async Task GetShoppingCartRequestHandlerTest_NewUserId()
+        {
+            // Arrange
+            var handler = new GetShoppingCartRequestHandler(CartHeader, CartDetails, ProductService, CouponService, Mapper);
+            var userId = "newuserid";
+
+            // Act
+            var result = await handler.Handle(new GetShoppingCartRequest
+            {
+                UserId = userId,
+            }, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.ErrorMessage.Should().BeNullOrEmpty();
+            result.ValidationErrors.Should().BeNullOrEmpty();
+            result.SuccessMessage.Should().Be("Ваша корзина пуста! Добавьте товары");
+        }
+    }
+}
