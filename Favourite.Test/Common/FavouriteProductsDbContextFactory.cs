4255784b2e0346bb6f067158b1fdcfec373e13b2
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 19:35:16 +0300

    Create FavouriteProductsDbContextFactory class

---
+++ Favourite.Test/Common/FavouriteProductsDbContextFactory.cs
@@ -0,0 +1,37 @@
+﻿using Favourite.Domain.Entities;
+using Favourite.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Favourite.Test.Common
+{
+    public static class FavouriteProductsDbContextFactory
+    {
+        public static ApplicationDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+            var context = new ApplicationDbContext(options);
+            context.Database.EnsureCreated();
+            context.FavouriteHeaders.AddRange(new FavouriteHeader
+            {
+                FavouriteHeaderId = 1,
+                UserId = "bestuserid1"
+            });
+
+            context.FavouriteDetails.AddRange(new FavouriteDetails
+            {
+                FavouriteDetailsId = 1,
+                FavouriteHeaderId = 1,
+                ProductId = 1,
+            });
+
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Destroy(ApplicationDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+    }
+}
