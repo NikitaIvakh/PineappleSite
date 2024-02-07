84d73b0120c0dd986e75ae0aeb3e05d31c726bc1
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 17:04:31 +0300

    Configure swagger

--- Favourite.API/Program.cs
+++ Favourite.API/Program.cs
@@ -1,5 +1,6 @@
 using Favourite.Application.DependencyInjection;
 using Favourite.Infrastructure.DependencyInjection;
+using Favourite.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.ConfigureApplicationServices();
 builder.Services.ConfigureInfrastructureServices(builder.Configuration);
+builder.Services.AddSwagger();
 
 builder.Services.AddHttpClient("Product", key => key.BaseAddress = new Uri(builder.Configuration["ServiceUrls:Product"]));
 
---
+++ Favourite.API/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Favourite.API
+{
+    public static class Startup
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwagger();
+            services.AddSwaggerGen();
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Микросервис для работы с корзиной",
+                    Description = "Микросервис для работы с корзиной",
+                    TermsOfService = new Uri("https://github.com/NikitaIvakh"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nikita Ivakh",
+                        Email = "nikita.ivakh7@gmail.com",
+                    },
+                });
+
+                options.SwaggerDoc("v2", new OpenApiInfo
+                {
+                    Version = "v2",
+                    Title = "Микросервис для работы с корзиной",
+                    Description = "Микросервис для работы с корзиной",
+                    TermsOfService = new Uri("https://github.com/NikitaIvakh"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nikita Ivakh",
+                        Email = "nikita.ivakh7@gmail.com",
+                    },
+                });
+            });
+        }
+    }
+}
