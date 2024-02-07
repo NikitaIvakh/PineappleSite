73af9a46403b2910151dacba27258468efd841cf
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-07 12:07:27 +0300

    Create ApplicationDbContext class

---
+++ Favourite.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Favourite.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Favourite.Infrastructure
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<FavouriteHeader> FavouriteHeaders { get; set; }
+
+        public DbSet<FavouriteDetails> FavouriteDetails { get; set; }
+    }
+}
