f862640c14e43d05dbab942ea2125308ede4c945
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 19:32:46 +0300

    Update command

--- Order.Application/Features/Handlers/Commands/UpdateOrderStatusRequestHandler.cs
+++ Order.Application/Features/Handlers/Commands/UpdateOrderStatusRequestHandler.cs
@@ -15,7 +15,6 @@ namespace Order.Application.Features.Handlers.Commands
     public class UpdateOrderStatusRequestHandler(IBaseRepository<OrderHeader> orderHeaderRepository, IMapper mapper) : IRequestHandler<UpdateOrderStatusRequest, Result<OrderHeaderDto>>
     {
         private readonly IBaseRepository<OrderHeader> _orderHeaderRepository = orderHeaderRepository;
-
         private readonly IMapper _mapper = mapper;
 
         public async Task<Result<OrderHeaderDto>> Handle(UpdateOrderStatusRequest request, CancellationToken cancellationToken)
