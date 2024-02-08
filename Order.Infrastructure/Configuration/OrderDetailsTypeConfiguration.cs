3238fd97a2ee0da8f481b8734f7b9e3cf2196ee4
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 11:05:05 +0300

    Create entity type configurations

---
+++ Order.Infrastructure/Configuration/OrderDetailsTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Order.Domain.Entities;
+
+namespace Order.Infrastructure.Configuration
+{
+    public class OrderDetailsTypeConfiguration : IEntityTypeConfiguration<OrderDetails>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetails> builder)
+        {
+            builder.HasKey(key => key.OrderDetailsId);
+            builder.HasOne(key => key.OrderHeader).WithMany().HasForeignKey(key => key.OrderHeaderId);
+
+            builder.Ignore(key => key.Product);
+        }
+    }
+}
---
+++ Order.Infrastructure/Configuration/OrderHeaderTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Order.Domain.Entities;
+
+namespace Order.Infrastructure.Configuration
+{
+    public class OrderHeaderTypeConfiguration : IEntityTypeConfiguration<OrderHeader>
+    {
+        public void Configure(EntityTypeBuilder<OrderHeader> builder)
+        {
+            builder.HasKey(key => key.OrderHeaderId);
+        }
+    }
+}
