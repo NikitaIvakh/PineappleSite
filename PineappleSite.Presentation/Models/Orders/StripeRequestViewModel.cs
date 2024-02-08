0d4169be6f201c0cc0f366cf69a447ca61871933
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 20:18:06 +0300

    Create new entity

---
+++ PineappleSite.Presentation/Models/Orders/StripeRequestViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PineappleSite.Presentation.Models.Orders
+{
+    public class StripeRequestViewModel
+    {
+        public string? StripeSessionUrl { get; set; }
+
+        public string? StripeSessionId { get; set; }
+
+        public string ApprovedUrl { get; set; }
+
+        public string CancelUrl { get; set; }
+
+        public OrderHeaderViewModel OrderHeader { get; set; }
+    }
+}
