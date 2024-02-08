93523b3f5c5e0c7fcf22626cddaa0a1e4807c323
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 12:43:07 +0300

    Create mapping

---
+++ Order.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Order.Domain.DTOs;
+using Order.Domain.Entities;
+
+namespace Order.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+            CreateMap<OrderDetails, OrderDetailsDto>().ReverseMap();
+        }
+    }
+}
