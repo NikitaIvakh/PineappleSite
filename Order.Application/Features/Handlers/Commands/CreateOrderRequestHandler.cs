09d7ab93c4c1d6eb702efaa7fc4b6c85d483cdc6
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 16:08:39 +0300

    Create command for create order

---
+++ Order.Application/Features/Handlers/Commands/CreateOrderRequestHandler.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MediatR;
+using Order.Application.Features.Requests.Commands;
+using Order.Application.Resources;
+using Order.Application.Utility;
+using Order.Domain.DTOs;
+using Order.Domain.Entities;
+using Order.Domain.Enum;
+using Order.Domain.Interfaces.Repository;
+using Order.Domain.ResultOrder;
+
+namespace Order.Application.Features.Handlers.Commands
+{
+    public class CreateOrderRequestHandler(IBaseRepository<OrderHeader> orderHeaderRepository, IMapper mapper) : IRequestHandler<CreateOrderRequest, Result<OrderHeaderDto>>
+    {
+        private readonly IBaseRepository<OrderHeader> _orderHeaderRepository = orderHeaderRepository;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Result<OrderHeaderDto>> Handle(CreateOrderRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(request.CartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.UtcNow;
+                orderHeaderDto.Status = StaticDetails.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(request.CartDto.CartDetails);
+                orderHeaderDto.OrderTotal = Math.Round(orderHeaderDto.OrderTotal, 2);
+
+                OrderHeader orderCreated = await _orderHeaderRepository.CreateAsync(_mapper.Map<OrderHeader>(orderHeaderDto));
+
+                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+
+                return new Result<OrderHeaderDto>
+                {
+                    Data = orderHeaderDto,
+                    SuccessMessage = "Заказ успешно создан",
+                };
+            }
+
+            catch (Exception exception)
+            {
+                return new Result<OrderHeaderDto>
+                {
+                    ErrorMessage = ErrorMessages.InternalServerError,
+                    ErrorCode = (int)ErrorCodes.InternalServerError,
+                    ValidationErrors = new List<string> { exception.Message },
+                };
+            }
+        }
+    }
+}
---
+++ Order.Application/Features/Requests/Commands/CreateOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Order.Domain.DTOs;
+using Order.Domain.ResultOrder;
+
+namespace Order.Application.Features.Requests.Commands
+{
+    public class CreateOrderRequest : IRequest<Result<OrderHeaderDto>>
+    {
+        public CartDto CartDto { get; set; }
+    }
+}
