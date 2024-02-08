8a61472c2d5e8a4445fa00fef3f7834303b70f34
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 20:26:53 +0300

    Create result classes

---
+++ PineappleSite.Presentation/Services/Orders/OrderCollectionResult.cs
@@ -0,0 +1,7 @@
+﻿namespace PineappleSite.Presentation.Services.Orders
+{
+    public class OrderCollectionResult<TEntity> : OrderResult<IReadOnlyCollection<TEntity>>
+    {
+        public int Count { get; set; }
+    }
+}
---
+++ PineappleSite.Presentation/Services/Orders/OrderResult.cs
@@ -0,0 +1,44 @@
+﻿namespace PineappleSite.Presentation.Services.Orders
+{
+    public class OrderResult
+    {
+        public bool IsSuccess => ErrorMessage == null;
+
+        public string? ErrorMessage { get; set; }
+
+        public string? SuccessMessage { get; set; }
+
+        public int? ErrorCode { get; set; }
+
+        public string? ValidationErrors { get; set; }
+    }
+
+    public class OrderResult<TEntity> : OrderResult
+    {
+        public OrderResult(string? errorMessage, int? errorCode, TEntity? data)
+        {
+            ErrorMessage = errorMessage;
+            ErrorCode = errorCode;
+            Data = data;
+        }
+
+        public OrderResult(string? errorMessage, int? errorCode, string? validationErrors)
+        {
+            ErrorMessage = errorMessage;
+            ErrorCode = errorCode;
+            ValidationErrors = validationErrors;
+        }
+
+        public OrderResult(TEntity? data)
+        {
+            Data = data;
+        }
+
+        public OrderResult()
+        {
+
+        }
+
+        public TEntity? Data { get; set; }
+    }
+}
