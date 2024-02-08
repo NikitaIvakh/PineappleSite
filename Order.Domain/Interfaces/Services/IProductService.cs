a33ac411c4a45080f35a03a82cbbba5ccb8b9e77
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 17:30:22 +0300

    Create service IProductService

---
+++ Order.Domain/Interfaces/Services/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using Order.Domain.DTOs;
+using Order.Domain.ResultOrder;
+
+namespace Order.Domain.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<CollectionResult<ProductDto>> GetProductListAsync();
+    }
+}
---
+++ Order.Infrastructure/Repository/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using Order.Domain.DTOs;
+using Order.Domain.Interfaces.Services;
+using Order.Domain.ResultOrder;
+using System.Text.Json;
+
+namespace Order.Infrastructure.Repository.Services
+{
+    public class ProductService(IHttpClientFactory httpClientFactory) : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+        public async Task<CollectionResult<ProductDto>> GetProductListAsync()
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient("Product");
+            var response = await httpClient.GetAsync($"/api/Product");
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var stream = await response.Content.ReadAsStreamAsync();
+                JsonSerializerOptions jsonSerializerOptions = new()
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+
+                var options = jsonSerializerOptions;
+                return await JsonSerializer.DeserializeAsync<CollectionResult<ProductDto>>(stream, options);
+            }
+
+            return new CollectionResult<ProductDto>();
+        }
+    }
+}
