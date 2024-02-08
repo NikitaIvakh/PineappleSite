2c330491a58925e3cdb83cb138f16e8a442ac088
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 11:00:49 +0300

    Create database context class

---
+++ Order.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Domain.Entities;
+
+namespace Order.Infrastructure
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
