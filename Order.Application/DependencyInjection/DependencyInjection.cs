21f453f94967058e113b7f8f8e2610c5a4fd41ed
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 12:40:49 +0300

    Create DependencyInjection class

---
+++ Order.Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Order.Application.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.RegisterInits();
+        }
+
+        private static void RegisterInits(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(config => config.RegisterServicesFromAssemblies([Assembly.GetExecutingAssembly()]));
+        }
+    }
+}
