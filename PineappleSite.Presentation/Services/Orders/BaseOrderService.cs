90e915ec274973ca3d6f51369be3c1d0d73cfa78
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 20:38:51 +0300

    Create BaseOrderService class

---
+++ PineappleSite.Presentation/Services/Orders/BaseOrderService.cs
@@ -0,0 +1,38 @@
+﻿using PineappleSite.Presentation.Contracts;
+using PineappleSite.Presentation.Models.Orders;
+using System.Net.Http.Headers;
+
+namespace PineappleSite.Presentation.Services.Orders
+{
+    public class BaseOrderService(ILocalStorageService localStorageService, IOrderClient orderClient)
+    {
+        private readonly ILocalStorageService _localStorageService = localStorageService;
+        private readonly IOrderClient _orderClient = orderClient;
+
+        protected OrderResult<OrderHeaderViewModel> ConvertOrderExceptions(OrdersExceptions ordersExceptions)
+        {
+            if (ordersExceptions.StatusCode == 400)
+            {
+                return new OrderResult<OrderHeaderViewModel>() { ErrorMessage = "Произошли ошибки валидации.", ErrorCode = 400, ValidationErrors = ordersExceptions.Response, };
+            }
+
+            else if (ordersExceptions.StatusCode == 404)
+            {
+                return new OrderResult<OrderHeaderViewModel>() { ErrorMessage = "Требуемый элемент не удалось найти.", ErrorCode = 404 };
+            }
+
+            else
+            {
+                return new OrderResult<OrderHeaderViewModel>() { ErrorMessage = "Что-то пошло не так, пожалуйста, попробуйте еще раз.", ErrorCode = 500 };
+            }
+        }
+
+        protected void AddBearerToken()
+        {
+            if (_localStorageService.Exists("token"))
+            {
+                _orderClient.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _localStorageService.GetStorageValue<string>("token"));
+            }
+        }
+    }
+}
