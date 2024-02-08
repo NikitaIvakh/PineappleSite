3d7b163c4691eedc2b5536f80576f9857b2cd069
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 20:16:12 +0300

    Create new entities

---
+++ PineappleSite.Presentation/Models/Orders/OrderDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PineappleSite.Presentation.Models.Products;
+
+namespace PineappleSite.Presentation.Models.Orders
+{
+    public class OrderDetailsViewModel
+    {
+        public int OrderDetailsId { get; set; }
+
+        public OrderHeaderViewModel? OrderHeader { get; set; }
+
+        public int OrderHeaderId { get; set; }
+
+        public ProductViewModel? Product { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Count { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+    }
+}
