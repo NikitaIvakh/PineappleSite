21535546b8d3ea25e8c24eb8af144cb555b372c4
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 13:05:08 +0300

    Create StaticDetails class

---
+++ Order.Application/Utility/StaticDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace Order.Application.Utility
+{
+    public class StaticDetails
+    {
+        public const string Status_Pending = "Ожидается";
+        public const string Status_Approved = "Подтвержден";
+        public const string Status_ReadyForPickup = "Готов к выдаче";
+        public const string Status_Completed = "Завершен";
+        public const string Status_Refunded = "Возвращен";
+        public const string Status_Cancelled = "Отменен";
+
+        public const string RoleAdmin = "Administrator";
+        public const string RoleEmployee = "Employee";
+        public const string UserAdmin = "admin@localhost.com";
+    }
+}
