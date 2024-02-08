e1ed59df5668345d8130142e2324fb31a4cd1357
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 11:14:26 +0300

    Implement IBaseRepository interface

---
+++ Order.Infrastructure/Repository/Implementation/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Domain.Interfaces.Repository;
+
+namespace Order.Infrastructure.Repository.Implementation
+{
+    public class BaseRepository<TEntity>(ApplicationDbContext context) : IBaseRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().AsNoTracking();
+        }
+
+        public Task<TEntity> CreateAsync(TEntity entity)
+        {
+            if (entity is not null)
+            {
+                _context.Add(entity);
+                _context.SaveChanges();
+
+                return Task.FromResult(entity);
+            }
+
+            throw new ArgumentNullException(nameof(entity), "Объект пустой");
+        }
+
+        public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            if (entity is not null)
+            {
+                _context.Update(entity);
+                _context.SaveChanges();
+            }
+
+            throw new ArgumentNullException(nameof(entity), "Объект пустой");
+        }
+
+        public Task DeleteAsync(TEntity entity)
+        {
+            if (entity is not null)
+            {
+                _context.Remove(entity);
+                _context.SaveChanges();
+
+                return Task.FromResult(entity);
+            }
+
+            throw new ArgumentNullException(nameof(entity), "Объект пустой");
+        }
+    }
+}
