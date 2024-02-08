685f83fd1681aaf45abd600124216a8d8b8a2f5d
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-08 15:26:22 +0300

    Create request for get orders

---
+++ Order.Application/Features/Handlers/Requests/GetOrderListRequestHandler.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Order.Application.Features.Requests.Requests;
+using Order.Application.Resources;
+using Order.Application.Utility;
+using Order.Domain.DTOs;
+using Order.Domain.Entities;
+using Order.Domain.Enum;
+using Order.Domain.Interfaces.Repository;
+using Order.Domain.ResultOrder;
+using Microsoft.EntityFrameworkCore;
+
+namespace Order.Application.Features.Handlers.Requests
+{
+    public class GetOrderListRequestHandler(IBaseRepository<OrderHeader> orderHeaderRepository, IHttpContextAccessor httpContextAccessor, IMapper mapper) : IRequestHandler<GetOrderListRequest, Result<OrderHeaderDto>>
+    {
+        private readonly IBaseRepository<OrderHeader> _orderHeaderRepository = orderHeaderRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<Result<OrderHeaderDto>> Handle(GetOrderListRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                IEnumerable<OrderHeader> orderHeader;
+                var userRole = _httpContextAccessor.HttpContext.User.IsInRole(StaticDetails.RoleAdmin);
+
+                if (userRole is true)
+                    orderHeader = _orderHeaderRepository.GetAll().Include(key => key.OrderDetails).OrderBy(key => key.OrderHeaderId).ToList();
+
+                else
+                    orderHeader = _orderHeaderRepository.GetAll().Include(key => key.OrderDetails).Where(key => key.UserId == request.UserId).OrderBy(key => key.OrderHeaderId).ToList();
+
+                return new Result<OrderHeaderDto>
+                {
+                    SuccessMessage = "Список заказов",
+                    Data = _mapper.Map<OrderHeaderDto>(orderHeader),
+                };
+            }
+
+            catch (Exception exception)
+            {
+                return new Result<OrderHeaderDto>
+                {
+                    ErrorMessage = ErrorMessages.InternalServerError,
+                    ErrorCode = (int)ErrorCodes.InternalServerError,
+                    ValidationErrors = new List<string> { exception.Message }
+                };
+            }
+        }
+    }
+}
---
+++ Order.Application/Features/Requests/Requests/GetOrderListRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Order.Domain.DTOs;
+using Order.Domain.ResultOrder;
+
+namespace Order.Application.Features.Requests.Requests
+{
+    public class GetOrderListRequest : IRequest<Result<OrderHeaderDto>>
+    {
+        public string UserId { get; set; }
+    }
+}
