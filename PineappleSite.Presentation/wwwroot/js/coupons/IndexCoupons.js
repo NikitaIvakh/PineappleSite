e59863c27477d68efdad552a5400b3c17fdb61c3
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-10 14:23:46 +0300

    Update coupon index

---
+++ PineappleSite.Presentation/wwwroot/js/coupons/IndexCoupons.js
@@ -0,0 +1,16 @@
+﻿
+document.getElementById("clearSearch").addEventListener("click", function () {
+    document.getElementById("searchCode").value = "";
+    window.location.href = "/Coupon/Index";
+});
+
+$(document).ready(function () {
+    $('input[name="selectedCoupons"]').change(function () {
+        var selectedCount = $('input[name="selectedCoupons"]:checked').length;
+        if (selectedCount >= 2) {
+            $('#deleteAllButton').show();
+        } else {
+            $('#deleteAllButton').hide();
+        }
+    });
+});
