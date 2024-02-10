dd3d096b4b26d85f0645caa5d41fade4c56a1431
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-10 14:44:04 +0300

    Update views for user profile

---
+++ PineappleSite.Presentation/wwwroot/js/users/editUser.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+    $('#togglePassword').click(function () {
+        var passwordInput = $('input[name="Password"]');
+        var passwordType = passwordInput.attr('type');
+
+        if (passwordType === 'password') {
+            passwordInput.attr('type', 'text');
+            $('#togglePassword i').removeClass('fa-eye').addClass('fa-eye-slash');
+            $('#togglePassword').text('Скрыть');
+        } else {
+            passwordInput.attr('type', 'password');
+            $('#togglePassword i').removeClass('fa-eye-slash').addClass('fa-eye');
+            $('#togglePassword').text('Показать');
+        }
+    });
+});
