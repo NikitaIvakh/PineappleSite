9079c05157f4625656783297a29ff461188cc3ab
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-14 16:51:23 +0300

    Update Serilog

--- Coupon.API/Program.cs
+++ Coupon.API/Program.cs
@@ -1,10 +1,8 @@
 using Coupon.API;
 using Coupon.Application.DependencyInjection;
 using Coupon.Infrastructure.DependencyInjection;
-using Microsoft.AspNetCore.Localization;
 using Serilog;
 using Stripe;
-using System.Globalization;
 
 WebApplicationBuilder applicationBuilder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +16,11 @@ applicationBuilder.Services.AddSwaggerGen();
 
 applicationBuilder.Services.ConfigureApplicationService();
 applicationBuilder.Services.ConfigureInfrastructureServive(applicationBuilder.Configuration);
-applicationBuilder.Host.UseSerilog((context, logConfig) => logConfig.ReadFrom.Configuration(context.Configuration));
+applicationBuilder.Host.UseSerilog((context, logConfig) =>
+{
+    logConfig.ReadFrom.Configuration(context.Configuration);
+    logConfig.WriteTo.Console();
+});
 
 StripeConfiguration.ApiKey = applicationBuilder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 
@@ -33,38 +35,7 @@ applicationBuilder.Services.AddCors(key =>
 
 applicationBuilder.Services.AddSwagger();
 
-applicationBuilder.Services.AddLocalization(options =>
-{
-    options.ResourcesPath = "Coupon.Application.Resources.Features.Coupons.Handlers.Commands";
-});
-
-applicationBuilder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    var supportedCultures = new[]
-    {
-        new CultureInfo("ru-RU"),
-        new CultureInfo("en-US"),
-    };
-
-    options.DefaultRequestCulture = new RequestCulture("ru-RU");
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-});
-
 WebApplication webApplication = applicationBuilder.Build();
-
-var supportedCultures = new[]
-{
-    new CultureInfo("ru-RU"),
-    new CultureInfo("en-US"),
-};
-
-webApplication.UseRequestLocalization(new RequestLocalizationOptions
-{
-    DefaultRequestCulture = new RequestCulture("ru-RU"),
-    SupportedCultures = supportedCultures,
-    SupportedUICultures = supportedCultures
-});
 
 // Configure the HTTP request pipeline.
 if (webApplication.Environment.IsDevelopment())
