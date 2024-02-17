10e181f35976b08dfb07580d70ee4a671b1abdd7
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-17 15:18:28 +0300

    Create DbConnectionFactory class

---
+++ Favourite.Infrastructure/Health/DbConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Favourite.Infrastructure.Health
+{
+    public class DbConnectionFactory(string connectionString)
+    {
+        private readonly string _connectionString = connectionString;
+
+        public IDbConnection OpenConnection()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
