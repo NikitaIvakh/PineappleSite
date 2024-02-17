a28478ed2eb0eb6d1a5f2d9eb4fdeafd9c98c46b
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-17 16:57:26 +0300

    Create DatabaseHealthCheck class

---
+++ ShoppingCart.Infrastructure/Health/DatabaseHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Npgsql;
+using System.Data;
+
+namespace ShoppingCart.Infrastructure.Health
+{
+    public class DatabaseHealthCheck(DbConnectionFactory connectionFactory) : IHealthCheck
+    {
+        private readonly DbConnectionFactory _connectionFactory = connectionFactory;
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using IDbConnection dbConnection = _connectionFactory.OpenConnection();
+                using var command = new NpgsqlCommand("SELECT 1", (NpgsqlConnection)dbConnection);
+                var result = await command.ExecuteScalarAsync(cancellationToken);
+
+                return result switch
+                {
+                    int and 1 => HealthCheckResult.Healthy(),
+                    _ => HealthCheckResult.Unhealthy(),
+                };
+            }
+
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy(exception: ex);
+            }
+        }
+    }
+}
