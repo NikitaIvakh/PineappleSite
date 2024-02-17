749b3691a3801075f48c7d874060d74f59f0996e
Nikita Ivakh <maffy145rus@mail.ru>
2024-02-17 16:58:51 +0300

    Implement dependency

--- ShoppingCart.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ ShoppingCart.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using ShoppingCart.Domain.Entities;
 using ShoppingCart.Domain.Interfaces.Repository;
 using ShoppingCart.Domain.Interfaces.Service;
+using ShoppingCart.Infrastructure.Health;
 using ShoppingCart.Infrastructure.Repository.Implement;
 using ShoppingCart.Infrastructure.Repository.Services;
 
@@ -13,18 +14,23 @@ namespace ShoppingCart.Infrastructure.DependencyInjection
     {
         public static void ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            services.RegisterServices();
+            services.RegisterServices(configuration);
             services.RegisterDataBase(configuration);
             services.ApplyMigration();
         }
 
-        private static void RegisterServices(this IServiceCollection services)
+        private static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var connectionString = configuration.GetConnectionString("ShoppingCartDbConnection");
+
             services.AddScoped<IBaseRepository<CartHeader>, BaseRepository<CartHeader>>();
             services.AddScoped<IBaseRepository<CartDetails>, BaseRepository<CartDetails>>();
 
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICouponService, CouponService>();
+
+            services.AddScoped(scope => new DbConnectionFactory(connectionString));
+            services.AddHealthChecks().AddNpgSql(connectionString).AddDbContextCheck<ApplicationDbContext>();
         }
 
         private static void RegisterDataBase(this IServiceCollection services, IConfiguration configuration)
