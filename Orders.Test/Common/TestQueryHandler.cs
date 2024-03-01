bb410f6799f8dc7307e64259681543c7797ab134
Nikita Ivakh <maffy145rus@mail.ru>
2024-03-01 10:10:18 +0300

    Create TestQueryHandler class

---
+++ Orders.Test/Common/TestQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Order.Application.Mapping;
+using Order.Infrastructure;
+
+namespace Orders.Test.Common
+{
+    [CollectionDefinition("QueryCollection")]
+    public class TestQueryHandler : IDisposable
+    {
+        protected ApplicationDbContext Context;
+        protected IMapper Mapper;
+
+        public TestQueryHandler()
+        {
+            Context = OrdersDbContextFactory.Create();
+
+            var mapperConfiguration = new MapperConfiguration(options =>
+            {
+                options.AddProfile<MappingProfile>();
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            OrdersDbContextFactory.Destroy(Context);
+        }
+    }
+
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<TestQueryHandler> { }
+}
