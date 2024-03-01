99f9ca46ae9e3d53c66650a1ae7ad55a726d0a8e
Nikita Ivakh <maffy145rus@mail.ru>
2024-03-01 11:12:57 +0300

    Create TestCommandsHandler class

---
+++ Orders.Test/Common/TestCommandsHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Caching.Memory;
+using Order.Application.Mapping;
+using Order.Domain.Entities;
+using Order.Domain.Interfaces.Repository;
+using Order.Infrastructure.Repository.Implementation;
+using Order.Infrastructure;
+
+namespace Orders.Test.Common
+{
+    public class TestCommandsHandler : IDisposable
+    {
+        protected ApplicationDbContext Context;
+        protected IMapper Mapper;
+        protected IMemoryCache MemoryCache;
+        protected IBaseRepository<OrderHeader> OrderHeader;
+        protected IBaseRepository<OrderDetails> OrderDetails;
+
+        public TestCommandsHandler()
+        {
+            Context = OrdersDbContextFactory.Create();
+
+            var mapperConfiguration = new MapperConfiguration(options =>
+            {
+                options.AddProfile<MappingProfile>();
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+            MemoryCache = new MemoryCache(new MemoryCacheOptions());
+            OrderHeader = new BaseRepository<OrderHeader>(Context);
+            OrderDetails = new BaseRepository<OrderDetails>(Context);
+        }
+
+        public void Dispose()
+        {
+            OrdersDbContextFactory.Destroy(Context);
+        }
+    }
+}
