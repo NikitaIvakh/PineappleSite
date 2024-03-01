ba3cdf7034f72e0db44b3db2255f977a7fb79775
Nikita Ivakh <maffy145rus@mail.ru>
2024-03-01 11:40:30 +0300

    Update validation. Add CreateOrderRequestHandler_FailOrWrongEmail method

--- Order.Application/Validators/IOrderValidator.cs
+++ Order.Application/Validators/IOrderValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Order.Domain.DTOs;
+using System.Net.Mail;
 using System.Text.RegularExpressions;
 
 namespace Order.Application.Validators
@@ -16,13 +17,21 @@ namespace Order.Application.Validators
             RuleFor(key => key.Email)
                 .NotEmpty().NotNull().WithMessage("Почтовый адрес не может быть пустым")
                 .MinimumLength(2).WithMessage("Почтовый адрес должен быть более 2 символов")
-                .MaximumLength(45).WithMessage("Почтовый адрес не должен превышать 45 символов");
+                .MaximumLength(45).WithMessage("Почтовый адрес не должен превышать 45 символов")
+                .MustAsync(BeValidEmailAddress).WithMessage("Введите действительный адрес электронной почты");
 
             RuleFor(key => key.PhoneNumber)
                 .NotEmpty().NotNull().WithMessage("Поле с номером телефона не может быть пустым")
                 .MinimumLength(2).WithMessage("Номер телефона должен быть более 7 символов")
                 .MaximumLength(15).WithMessage("Номер телефона не должен превышать 12 символов")
                 .MustAsync(BeValidMobilePhoneOrStandartPhone).WithMessage("Номер телефона не верного формата");
+        }
+
+        private Task<bool> BeValidEmailAddress(string emailAddress, CancellationToken token)
+        {
+            string emailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            bool isValid = Regex.IsMatch(emailAddress, emailPattern);
+            return Task.FromResult(isValid);
         }
 
         private Task<bool> BeValidMobilePhoneOrStandartPhone(string phoneNumber, CancellationToken token)
