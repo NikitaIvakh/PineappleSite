b28d47247ab01950b1688ccc526a3cb2a4ef62a9
Nikita Ivakh <maffy145rus@mail.ru>
2024-03-08 18:47:21 +0300

    Change the structure of the project

---
+++ Services/Prodict/Product.Infrastructure/Migrations/20240308153848_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Product.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ProductCategory = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: true),
+                    ImageLocalPath = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageLocalPath", "ImageUrl", "Name", "Price", "ProductCategory" },
+                values: new object[,]
+                {
+                    { 1, "Самый вкусный суп.", null, "https://placehold.co/600x400", "Борщ", 10.0, "Soups" },
+                    { 2, "Сырные шарики с крабовыми палочками.", null, "https://placehold.co/601x401", "Сырные шарики", 15.0, "Snacks" },
+                    { 3, "Газированный напиток.", null, "https://placehold.co/602x402", "Кока-кола", 5.0, "Drinks" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
