4cc43905b7100ebff7cc12f978a83edf05b62989
Nikita Ivakh <maffy145rus@mail.ru>
2024-03-23 09:51:32 +0300

    Create GetUserDto class

---
+++ Services/Identity/Identity.Domain/DTOs/Identities/GetUserDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Identity.Domain.DTOs.Identities
+{
+    public record class GetUserDto(string UserId, string FirstName, string LastName, string UserName, string EmailAddress, DateTime? CreatedTime, DateTime? ModifiedTime);
+}
