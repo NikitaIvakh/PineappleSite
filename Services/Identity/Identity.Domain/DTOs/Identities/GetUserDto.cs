817130459ec7a598a1b3034e97d59906042d4189
Nikita Ivakh <maffy145rus@mail.ru>
2024-03-23 09:53:55 +0300

    Create GetAllUsersDto class. Add Role field

--- Services/Identity/Identity.Domain/DTOs/Identities/GetUserDto.cs
+++ Services/Identity/Identity.Domain/DTOs/Identities/GetUserDto.cs
@@ -1,4 +1,4 @@
 ﻿namespace Identity.Domain.DTOs.Identities
 {
-    public record class GetUserDto(string UserId, string FirstName, string LastName, string UserName, string EmailAddress, DateTime? CreatedTime, DateTime? ModifiedTime);
+    public record class GetUserDto(string UserId, string FirstName, string LastName, string UserName, string EmailAddress, IList<string> Role, DateTime? CreatedTime, DateTime? ModifiedTime);
 }
