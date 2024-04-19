0ae22752c61f455e0c229c7f3cb448b3cf3abff9
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-19 12:04:43 +0300

    Add Migration

---
+++ Services/Coupon/Coupon.Infrastructure/Migrations/20240419090314_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coupon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    CouponId = table.Column<string>(type: "text", nullable: false),
+                    CouponCode = table.Column<string>(type: "text", nullable: false),
+                    DiscountAmount = table.Column<double>(type: "numeric(10,2)", nullable: false),
+                    MinAmount = table.Column<double>(type: "numeric(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.CouponId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
