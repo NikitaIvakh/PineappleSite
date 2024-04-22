69d85e6071cc7d4f57ceab440289e9a48ca0397e
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-22 20:13:14 +0300

    Update file

---
+++ Services/Identity/Identity.API/Endpoints/UserEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using Carter;
+using Identity.Application.Features.Users.Requests.Queries;
+using Identity.Domain.DTOs.Identities;
+using Identity.Domain.ResultIdentity;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Identity.API.Endpoints;
+
+public class UserEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("api/users");
+
+        group.MapGet("/GetUsers", GetUsers).WithName(nameof(GetUsers));
+    }
+
+    private static async Task<Results<Ok<CollectionResult<GetUsersDto>>, BadRequest<string>>> GetUsers(ISender sender,
+        ILogger<GetUsersDto> logger)
+    {
+        var request = await sender.Send(new GetUsersRequest());
+
+        if (request.IsSuccess)
+        {
+            logger.LogDebug($"LogDebug ================ Пользователи успешно получены");
+            return TypedResults.Ok(request);
+        }
+
+        logger.LogError($"LogDebugError ================ Получить пользователей не удалось");
+        return TypedResults.BadRequest(string.Join(", ", request.ValidationErrors!));
+    }
+}
