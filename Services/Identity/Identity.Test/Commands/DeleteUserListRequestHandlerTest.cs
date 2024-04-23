a9ce5a69ab7ff3d78dd8dceb55ce0cecf03b0fbb
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-23 19:47:36 +0300

    Update test for delete users

--- Services/Identity/Identity.Test/Commands/DeleteUserListRequestHandlerTest.cs
+++ Services/Identity/Identity.Test/Commands/DeleteUserListRequestHandlerTest.cs
@@ -1,58 +1,48 @@
-﻿//using FluentAssertions;
-//using Identity.Application.Features.Identities.Commands.Commands;
-//using Identity.Application.Features.Identities.Requests.Commands;
-//using Identity.Domain.DTOs.Identities;
-//using Identity.Test.Common;
-//using Xunit;
+﻿using FluentAssertions;
+using Identity.Application.Features.Users.Commands.Handlers;
+using Identity.Application.Features.Users.Requests.Handlers;
+using Identity.Domain.DTOs.Identities;
+using Identity.Test.Common;
+using Xunit;
 
-//namespace Identity.Test.Commands
-//{
-//    public class DeleteUserListRequestHandlerTest : TestCommandHandler
-//    {
-//        [Fact]
-//        public async Task DeleteUserListRequestHandlerTest_Success()
-//        {
-//            // Arrange
-//            var handler = new DeleteUsersRequestHandler(UserManager, DeleteUsers, DeleteUserListLogger, Mapper);
-//            var deleteUserList = new DeleteUserListDto()
-//            {
-//                UserIds = ["8e445865-a24d-4543-a6c6-9443d048cdb9", "9e224968-33e4-4652-b7b7-8574d048cdb9"],
-//            };
+namespace Identity.Test.Commands
+{
+    public class DeleteUserListRequestHandlerTest : TestCommandHandler
+    {
+        [Fact]
+        public async Task DeleteUserListRequestHandlerTest_Success()
+        {
+            // Arrange
+            var handler = new DeleteUsersRequestHandler(UserRepository, DeleteUsers, MemoryCache);
+            var deleteUserList = new DeleteUsersDto(new List<string>()
+                { "8e445865-a24d-4543-a6c6-9443d048cdb9", "9e224968-33e4-4652-b7b7-8574d048cdb9" });
 
-//            // Act
-//            var result = await handler.Handle(new DeleteUsersRequest
-//            {
-//                DeleteUserList = deleteUserList,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteUsersRequest(deleteUserList), CancellationToken.None);
 
-//            // Assert
-//            result.IsSuccess.Should().BeTrue();
-//            result.Count.Should().Be(2);
-//            result.SuccessMessage.Should().Be("Пользователи успешно удалены");
-//            result.ErrorMessage.Should().BeNullOrEmpty();
-//            result.ValidationErrors.Should().BeNullOrEmpty();
-//        }
+            // Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Count.Should().Be(2);
+            result.SuccessMessage.Should().Be("Пользователи успешно удалены");
+            result.ErrorMessage.Should().BeNullOrEmpty();
+            result.ValidationErrors.Should().BeNullOrEmpty();
+        }
 
-//        [Fact]
-//        public async Task DeleteUserListRequestHandlerTest_Success_FailOrWrongIds()
-//        {
-//            // Arrange
-//            var handler = new DeleteUsersRequestHandler(UserManager, DeleteUsers, DeleteUserListLogger, Mapper);
-//            var deleteUserList = new DeleteUserListDto()
-//            {
-//                UserIds = [],
-//            };
+        [Fact]
+        public async Task DeleteUserListRequestHandlerTest_Success_FailOrWrongIds()
+        {
+            // Arrange
+            var handler = new DeleteUsersRequestHandler(UserRepository, DeleteUsers, MemoryCache);
+            var deleteUserList = new DeleteUsersDto(new List<string>()
+                { "8e445865-a24d-4543-a6c6-9443d048cdb1", "9e224968-33e4-4652-b7b7-8574d048cdb2" });
 
-//            // Act
-//            var result = await handler.Handle(new DeleteUsersRequest
-//            {
-//                DeleteUserList = deleteUserList,
-//            }, CancellationToken.None);
+            // Act
+            var result = await handler.Handle(new DeleteUsersRequest(deleteUserList), CancellationToken.None);
 
-//            // Assert
-//            result.IsSuccess.Should().BeFalse();
-//            result.ErrorMessage.Should().Be("Пользователи не могут быть удалены");
-//            result.SuccessMessage.Should().BeNullOrEmpty();
-//        }
-//    }
-//}
+            // Assert
+            result.IsSuccess.Should().BeFalse();
+            result.ErrorMessage.Should().Be("Пользователи не найдены");
+            result.SuccessMessage.Should().BeNullOrEmpty();
+        }
+    }
+}
