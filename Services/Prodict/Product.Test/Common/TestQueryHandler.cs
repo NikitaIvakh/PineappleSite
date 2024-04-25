a2aa73f40108d22ae433c014c81ce04877c78049
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-25 11:23:19 +0300

    Update GetProductListRequestHanlderTest class

--- Services/Prodict/Product.Test/Common/TestQueryHandler.cs
+++ Services/Prodict/Product.Test/Common/TestQueryHandler.cs
@@ -1,46 +1,32 @@
-﻿// using AutoMapper;
-// using Microsoft.Extensions.Caching.Memory;
-// using Moq;
-// using Product.Application.Features.Commands.Queries;
-// using Product.Application.Mapping;
-// using Product.Domain.Entities.Producrs;
-// using Product.Domain.Interfaces;
-// using Product.Infrastructure;
-// using Product.Infrastructure.Repository;
-// using Serilog;
-// using Xunit;
-//
-// namespace Product.Test.Common
-// {
-//     public class TestQueryHandler : IDisposable
-//     {
-//         protected ApplicationDbContext Context;
-//         protected IMapper Mapper;
-//         protected IProductRepository<ProductEntity> Repository;
-//         protected ILogger Logger;
-//         protected IMemoryCache MemoryCache;
-//
-//         public TestQueryHandler()
-//         {
-//             Context = ProductDbContextFactory.Create();
-//             Repository = new ProductRepository<ProductEntity>(Context);
-//             Logger = Log.ForContext<GetProductsRequestHandler>();
-//
-//             var mapperConfiguration = new MapperConfiguration(config =>
-//             {
-//                 config.AddProfile<MappingProfile>();
-//             });
-//
-//             Mapper = mapperConfiguration.CreateMapper();
-//             MemoryCache = new MemoryCache(new MemoryCacheOptions());
-//         }
-//
-//         public void Dispose()
-//         {
-//             ProductDbContextFactory.Destroy(Context);
-//         }
-//     }
-//
-//     [CollectionDefinition("QueryCollection")]
-//     public class QueryCollection : ICollectionFixture<TestQueryHandler> { }
-// }
+﻿using AutoMapper;
+using Microsoft.Extensions.Caching.Memory;
+using Product.Application.Mapping;
+using Product.Domain.Interfaces;
+using Product.Infrastructure;
+using Product.Infrastructure.Repository;
+
+namespace Product.Test.Common;
+
+public class TestQueryHandler : IDisposable
+{
+    protected ApplicationDbContext Context;
+    protected IMapper Mapper;
+    protected IProductRepository Repository;
+    protected IMemoryCache MemoryCache;
+
+    protected TestQueryHandler()
+    {
+        Context = ProductDbContextFactory.Create();
+        Repository = new ProductRepository(Context);
+
+        var mapperConfiguration = new MapperConfiguration(config => { config.AddProfile<MappingProfile>(); });
+
+        Mapper = mapperConfiguration.CreateMapper();
+        MemoryCache = new MemoryCache(new MemoryCacheOptions());
+    }
+
+    public void Dispose()
+    {
+        ProductDbContextFactory.Destroy(Context);
+    }
+}
---
+++ Services/Prodict/Product.Test/Queries/GetProductsRequestHanlderTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Product.Application.Features.Commands.Queries;
+using Product.Application.Features.Requests.Queries;
+using Product.Test.Common;
+using Shouldly;
+using Xunit;
+
+namespace Product.Test.Queries;
+
+public sealed class GetProductListRequestHanlderTest : TestQueryHandler
+{
+    [Fact]
+    public async Task GetProductListRequestHanlderTest_Success()
+    {
+        // Arrange
+        var handler = new GetProductsRequestHandler(Repository, MemoryCache);
+
+        // Act
+        var result = await handler.Handle(new GetProductsRequest(), CancellationToken.None);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.Count.Should().Be(6);
+    }
+}
