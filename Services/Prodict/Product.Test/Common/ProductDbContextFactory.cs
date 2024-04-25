1d16089db6ac94f7899567d3b5a499dd4c4aaa44
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-25 11:21:33 +0300

    Update ProductDbContextFactory class

--- Services/Prodict/Product.Test/Common/ProductDbContextFactory.cs
+++ Services/Prodict/Product.Test/Common/ProductDbContextFactory.cs
@@ -3,51 +3,49 @@ using Product.Domain.Entities.Producrs;
 using Product.Domain.Enum;
 using Product.Infrastructure;
 
-namespace Product.Test.Common
+namespace Product.Test.Common;
+
+public static class ProductDbContextFactory
 {
-    public static class ProductDbContextFactory
+    public static ApplicationDbContext Create()
     {
-        public static ApplicationDbContext Create()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
-            var context = new ApplicationDbContext(options);
-            context.Database.EnsureCreated();
-            context.AddRange(
-                new ProductEntity
-                {
-                    Id = 4,
-                    Name = "Test 1",
-                    Description = "Test product 1",
-                    ProductCategory = ProductCategory.Soups,
-                    Price = 10,
-                },
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        var context = new ApplicationDbContext(options);
+        context.Database.EnsureCreated();
+        context.AddRange(
+            new ProductEntity
+            {
+                Id = 4,
+                Name = "Test 1",
+                Description = "Test product 1",
+                ProductCategory = ProductCategory.Soups,
+                Price = 10,
+            },
+            new ProductEntity
+            {
+                Id = 5,
+                Name = "Test 2",
+                Description = "Test product 2",
+                ProductCategory = ProductCategory.Snacks,
+                Price = 15,
+            },
+            new ProductEntity
+            {
+                Id = 6,
+                Name = "Test 3",
+                Description = "Test product 3",
+                ProductCategory = ProductCategory.Drinks,
+                Price = 9,
+            });
 
-                new ProductEntity
-                {
-                    Id = 5,
-                    Name = "Test 2",
-                    Description = "Test product 2",
-                    ProductCategory = ProductCategory.Snacks,
-                    Price = 15,
-                },
+        context.SaveChanges();
+        return context;
+    }
 
-                new ProductEntity
-                {
-                    Id = 6,
-                    Name = "Test 3",
-                    Description = "Test product 3",
-                    ProductCategory = ProductCategory.Drinks,
-                    Price = 9,
-                });
-
-            context.SaveChanges();
-            return context;
-        }
-
-        public static void Destroy(ApplicationDbContext context)
-        {
-            context.Database.EnsureDeleted();
-            context.Dispose();
-        }
+    public static void Destroy(ApplicationDbContext context)
+    {
+        context.Database.EnsureDeleted();
+        context.Dispose();
     }
 }
