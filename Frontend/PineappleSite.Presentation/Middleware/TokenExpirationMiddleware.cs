80e8da85ccfcf99be41b4ade4eb5fce33f0d33f1
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-25 15:18:18 +0300

    Update Middleware

--- Frontend/PineappleSite.Presentation/Middleware/TokenExpirationMiddleware.cs
+++ Frontend/PineappleSite.Presentation/Middleware/TokenExpirationMiddleware.cs
@@ -8,17 +8,20 @@ public sealed class TokenExpirationMiddleware(RequestDelegate next)
     public async Task Invoke(HttpContext context)
     {
         if (!context.User.Identity!.IsAuthenticated && !context.Request.Cookies.ContainsKey("JWTToken") &&
-            context.Request.Path.Value is not "/Authenticate/Login")
+            context.Request.Path.Value is not "/Authenticate/Login" &&
+            context.Request.Path.Value is not "/Authenticate/Register" && context.Request.Path.Value is not "/")
         {
             await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            context.Response.Redirect("/Authenticate/Login");
+            context.Response.Redirect("/");
             return;
         }
 
-        if (!context.Request.Cookies.ContainsKey("JWTToken") && context.Request.Path.Value is not "/Authenticate/Login")
+        if (!context.Request.Cookies.ContainsKey("JWTToken") &&
+            context.Request.Path.Value is not "/Authenticate/Login" &&
+            context.Request.Path.Value is not "/Authenticate/Register" && context.Request.Path.Value is not "/")
         {
             await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            context.Response.Redirect("/Authenticate/Login");
+            context.Response.Redirect("/");
             return;
         }
 
