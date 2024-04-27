65c3fc8e36665345dad59387854be9822a34aa69
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-27 17:21:38 +0300

    Update UpdateOrderStatusRequestHandlerTest tests

--- Services/Order/Orders.Test/Commands/UpdateOrderStatusRequestHandlerTest.cs
+++ Services/Order/Orders.Test/Commands/UpdateOrderStatusRequestHandlerTest.cs
@@ -1,39 +1,55 @@
-﻿// using FluentAssertions;
-// using Microsoft.EntityFrameworkCore;
-// using Order.Application.Features.Handlers.Commands;
-// using Order.Application.Features.Requests.Commands;
-// using Order.Application.Utility;
-// using Orders.Test.Common;
-//
-// namespace Orders.Test.Commands;
-//
-// public class UpdateOrderStatusRequestHandlerTest : TestCommandsHandler
-// {
-//     [Fact]
-//     public async Task UpdateOrderStatusRequestHandler_Success()
-//     {
-//         // Arrange
-//         var handler = new UpdateOrderStatusRequestHandler(OrderHeader, Mapper, MemoryCache);
-//         const int orderId = 1;
-//         const string orderStatus = StaticDetails.StatusApproved;
-//
-//         foreach (var entity in Context.ChangeTracker.Entries())
-//         {
-//             entity.State = EntityState.Detached;
-//         }
-//
-//         //Act
-//         var result = await handler.Handle(new UpdateOrderStatusRequest()
-//         {
-//             OrderHeaderId = orderId,
-//             NewStatus = orderStatus,
-//         }, CancellationToken.None);
-//
-//         // Assert
-//         result.SuccessCode.Should().Be(203);
-//         result.SuccessMessage.Should().Be("Статус заказа успешно обновлен");
-//         result.ErrorCode.Should().BeNull();
-//         result.ErrorMessage.Should().BeNull();
-//         result.ValidationErrors.Should().BeNullOrEmpty();
-//     }
-// }
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Order.Application.Features.Handlers.Commands;
+using Order.Application.Features.Requests.Commands;
+using Order.Application.Utility;
+using Order.Domain.DTOs;
+using Orders.Test.Common;
+
+namespace Orders.Test.Commands;
+
+public sealed class UpdateOrderStatusRequestHandlerTest : TestCommandsHandler
+{
+    [Fact]
+    public async Task UpdateOrderStatusRequestHandler_Success()
+    {
+        // Arrange
+        var handler = new UpdateOrderStatusRequestHandler(OrderHeader, Mapper, MemoryCache);
+        const int orderId = 1;
+        const string orderStatus = StaticDetails.StatusApproved;
+        var updateOrderStatusdto = new UpdateOrderStatusDto(orderId, orderStatus);
+
+        foreach (var entity in Context.ChangeTracker.Entries())
+        {
+            entity.State = EntityState.Detached;
+        }
+
+        //Act
+        var result = await handler.Handle(new UpdateOrderStatusRequest(updateOrderStatusdto), CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.StatusCode.Should().Be(203);
+        result.SuccessMessage.Should().Be("Статус заказа успешно обновлен");
+    }
+    
+    [Fact]
+    public async Task UpdateOrderStatusRequestHandler_Success_FailOrWrong_OrderId()
+    {
+        // Arrange
+        var handler = new UpdateOrderStatusRequestHandler(OrderHeader, Mapper, MemoryCache);
+        const int orderId = 1999;
+        const string orderStatus = StaticDetails.StatusApproved;
+        var updateOrderStatusdto = new UpdateOrderStatusDto(orderId, orderStatus);
+
+
+        //Act
+        var result = await handler.Handle(new UpdateOrderStatusRequest(updateOrderStatusdto), CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.StatusCode.Should().Be(404);
+        result.ErrorMessage.Should().Be("Заголовок заказов не найден");
+        result.ValidationErrors.Should().Equal("Заголовок заказов не найден");
+    }
+}
