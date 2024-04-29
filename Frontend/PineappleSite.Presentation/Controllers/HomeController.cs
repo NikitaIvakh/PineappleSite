3103e9d46a905bc233de41b5b79ce360e16c6d1f
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-29 17:20:57 +0300

    Add logic for contacts

--- Frontend/PineappleSite.Presentation/Controllers/HomeController.cs
+++ Frontend/PineappleSite.Presentation/Controllers/HomeController.cs
@@ -225,6 +225,11 @@ public sealed class HomeController(
         return View();
     }
     
+    public IActionResult Contacts()
+    {
+        return View();
+    }
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
