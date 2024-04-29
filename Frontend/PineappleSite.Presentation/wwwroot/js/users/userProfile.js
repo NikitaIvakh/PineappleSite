92e9fcf1bd6c969d01bc37986cf6145c3a6f9632
Nikita Ivakh <maffy145rus@mail.ru>
2024-04-29 19:37:59 +0300

    Add localize for user profile

--- Frontend/PineappleSite.Presentation/wwwroot/js/users/userProfile.js
+++ Frontend/PineappleSite.Presentation/wwwroot/js/users/userProfile.js
@@ -1,16 +1,18 @@
-﻿$(document).ready(function () {
+﻿
+$(document).ready(function () {
     $('#togglePassword').click(function () {
-        var passwordInput = $('input[name="Password"]');
-        var passwordType = passwordInput.attr('type');
+        let passwordInput = $('input[name="Password"]');
+        let passwordType = passwordInput.attr('type');
+        let showText = $('#togglePassword').data('show-text');
 
         if (passwordType === 'password') {
             passwordInput.attr('type', 'text');
             $('#togglePassword i').removeClass('fa-eye').addClass('fa-eye-slash');
-            $('#togglePassword').text('Скрыть');
+            $('#togglePassword').text(showText);
         } else {
             passwordInput.attr('type', 'password');
             $('#togglePassword i').removeClass('fa-eye-slash').addClass('fa-eye');
-            $('#togglePassword').text('Показать');
+            $('#togglePassword').text(showText);
         }
     });
 });
